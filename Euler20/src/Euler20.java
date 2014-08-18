5438b2409a1391e1e6b3f3465921281d1862e7c8
ddsnowboard <ddsnowboard@gmail.com>
2014-08-17 21:30:38 -0500

    Did Euler20.
    I was just looking for somthing to do, so I did a random one of these.
    In other news, 10! is a pretty big number...

---
+++ Euler20/src/Euler20.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+
+
+public class Euler20 {
+
+	public static void main(String[] args) {
+		BigInteger factorial = fact(100);
+		System.out.println(factorial);
+		String factString = String.valueOf(factorial);
+		int total = 0;
+		for(int i = 0;i<factString.length();i++)
+		{
+			total+=Integer.parseInt(String.valueOf(factString.charAt(i)));
+		}
+		System.out.println(total);
+	}
+
+	private static BigInteger fact(int i) {
+		BigInteger end = new BigInteger(String.valueOf(1));
+		for(int p = i;p>0;p--)
+		{
+			end = end.multiply(BigInteger.valueOf(p));
+		}
+		return end;
+	}
+
+}
