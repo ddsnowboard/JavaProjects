25924854288f012d87851646bd37a6f0222cb27a
ddsnowboard <ddsnowboard@gmail.com>
2014-09-20 00:34:09 -0500

    Moved forward on Euler3, but I couldn't get it to work.
    See comment at the top of the file.

--- Euler3/src/Euler3.java
+++ Euler3/src/Euler3.java
@@ -1,40 +1,46 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+// Dear future me, 
+// This takes a long time and it always kicks me an OutOfMemoryError, so I think it might work in theory
+// but I can't get it to actually work. Maybe on my faster computer. O well. 
 
 public class Euler3 {
 
 	public static void main(String[] args) {
 		long num=600851475143l;
-		List <Integer> primes = sieve((long)(num/2)+1);
+		List<Long> primes = sieve((long)(num/2)+1);
+//		List<Long> primes = sieve((long)(10000));
 		Collections.reverse(primes);
-		for (int i : primes)
+//		System.out.println(primes.get(0));
+		for (long i : primes)
 		{
 			if (i%num == 0)
 			{
-				System.out.print(i);
+				System.out.printf("The largest prime factor is %d", i);
 				return;
 			}
 		}
 	}
-	public static List<Integer> sieve(long j)
+	public static List<Long> sieve(long j)
 	{
-		List <Integer>primes = new ArrayList<Integer>();
-		int p;
-		for(int i=2;i<=j;i++)
+		List<Long>primes = new ArrayList<Long>();
+		System.out.println("Did something");
+		primes.add(2l);
+		for(long i=3;i<=j;i+=2)
 		{
 			primes.add(i);
 		}
-		for (int i = 0;i<primes.size();i++)
+		long p = 0;
+		List<Long> primeprime = primes;
+		for (int i = 0;i<(long)(Math.sqrt(primes.size())+1);i++)
 		{
-			
 			p=primes.get(i);
 			for(int q=2;q<j;q++)
 			{
-				if(primes.contains(p*q))
+				if(primeprime.contains(p*q))
 				{
-					primes.remove((p*q));
+					primeprime.remove(primeprime.indexOf(p*q));
 				}
 			}
 		}
