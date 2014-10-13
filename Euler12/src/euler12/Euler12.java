3c7df3db070ba07f61ce25270a9f742d27c47823
ddsnowboard <ddsnowboard@gmail.com>
2014-10-13 18:50:20 -0500

    Kept moving on Euler 12
    It is really, really slow. I need to find a better way to do it. Maybe I could get the triangle numbers faster.

--- Euler12/src/euler12/Euler12.java
+++ Euler12/src/euler12/Euler12.java
@@ -1,7 +1,5 @@
 package euler12;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author ddsnowboard
@@ -12,11 +10,31 @@ public class Euler12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        long index = 1;
+        long thisTriangle = triangle(index);
+        while (factors(thisTriangle) < 500) {
+            index++;
+            thisTriangle = triangle(index);
+        }
+        System.out.println(thisTriangle);
     }
-    public static ArrayList<Integer> factors(int n)
-    {
-        for(int i = 0;i<=n/2;n)
+
+    public static int factors(long n) {
+        int out = 0;
+        for (long i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                out++;
+            }
+        }
+        out++; // For the number itself
+        return out;
     }
-    
+
+    public static int triangle(long index) {
+        int out = 0;
+        for (int i = 0; i <= index; i++) {
+            out += i;
+        }
+        return out;
+    }
 }
