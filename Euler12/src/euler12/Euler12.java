7bd65f0cae20e1f46afd532d999c78aa7cf8ce74
ddsnowboard <ddsnowboard@gmail.com>
2014-10-13 14:07:59 -0500

    Started Euler12

---
+++ Euler12/src/euler12/Euler12.java
@@ -0,0 +1,22 @@
+package euler12;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ddsnowboard
+ */
+public class Euler12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    public static ArrayList<Integer> factors(int n)
+    {
+        for(int i = 0;i<=n/2;n)
+    }
+    
+}
