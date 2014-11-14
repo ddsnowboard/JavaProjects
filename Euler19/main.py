c20f7ec1fdd1661f92530344070e09f57997e456
ddsnowboard <ddsnowboard@gmail.com>
2014-11-14 15:05:29 -0600

    Finish Euler18
    I had the problem understood wrong. I thought I was supposed to find
    all the Sundays, not just the ones on the first of the month. Change the
    list to be of lists and make the second element in each be a boolean of
    whether that was the first of the month. No big deal.

--- Euler19/main.py
+++ Euler19/main.py
@@ -8,16 +8,18 @@ weekdays = weekdays()
 next_day = lambda: next(weekdays)
 days_total = []
 # This is the months in order with a function that returns the amount of days in them, 
-# with the input to the function being the year. For every month other than Februray,it doesn't
+# with the input to the function being the year. For every month other than February,it doesn't
 # do anything, but in February it tells whether the year is a leap year. 
 months = [lambda x: 31, lambda x: 29 if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0) else 28, lambda x: 31, lambda x:30, lambda x: 31, lambda x : 30, lambda x: 31, lambda x: 31, lambda x: 30, lambda x: 31, lambda x: 30, lambda x: 31]
-for i in range(1900, 2001):
+for i in range(1901, 2001):
 	for j in months:
 		for p in range(j(i)):
-			days_total.append(next_day())
+			if p == 0:
+				days_total.append([next_day(), True])
+			else:
+				days_total.append([next_day(), False])
 number = 0
 for i in days_total:
-	if i == "Sunday":
+	if i[0] == "Sunday" and i[1]:
 		number += 1
 print(number)
-print(len(days_total))
