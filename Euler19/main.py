cbe30a1dfa1cb040fe0bf531794717b8ced70a87
ddsnowboard <ddsnowboard@gmail.com>
2014-11-14 13:50:21 -0600

    Tried to fix it. Failed

--- Euler19/main.py
+++ Euler19/main.py
@@ -8,7 +8,7 @@ weekdays = weekdays()
 next_day = lambda: next(weekdays)
 days_total = []
 months = [lambda x: 31, lambda x: 29 if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0) else 28, lambda x: 31, lambda x:30, lambda x: 31, lambda x : 30, lambda x: 31, lambda x: 31, lambda x: 30, lambda x: 31, lambda x: 30, lambda x: 31]
-for i in range(1901, 2000):
+for i in range(1900, 2001):
 	for j in months:
 		for p in range(j(i)):
 			days_total.append(next_day())
@@ -17,4 +17,4 @@ for i in days_total:
 	if i == "Sunday":
 		number += 1
 print(number)
-print([i for i in range(1900, 2001) if months[1](i) == 29])
+print(len(days_total))
