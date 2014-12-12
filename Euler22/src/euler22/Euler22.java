c8b7ba0ed53f0412bf4e825237cb69770377ff1c
ddsnowboard <ddsnowboard@gmail.com>
2014-12-11 21:00:00 -0600

    Finished Euler22
    That wasn't hard. Why did I have so much trouble...

--- Euler22/src/euler22/Euler22.java
+++ Euler22/src/euler22/Euler22.java
@@ -8,8 +8,10 @@ package euler22;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -24,22 +26,24 @@ public class Euler22 {
         int total = 0;
         int currScore;
         String currName;
+        Pattern quote = Pattern.compile("\"");
         File f = new File("input.txt");
-        ArrayList<String> names = new ArrayList<>(6000);
+        String[] names;
         try (Scanner s = new Scanner(f)) {
-            names = 
+            names = s.nextLine().replace("\"", "").split(",");
         }
-        Collections.sort(names);
-        System.out.println(names.get(0));
-//        for (int i = 0; i < names.size(); i++) {
-//            currScore = 0;
-//            currName = names.get(i);
-//            for (int j = 0; j < currName.length(); j++) {
-//                currScore += currName.charAt(j);
-//            }
-//            total += currScore * (i + 1);
-//        }
-//        System.out.println(total);
+        Arrays.sort(names);
+        for (int i = 0; i < names.length; i++) {
+            currScore = 0;
+            currName = names[i];
+            for (int j = 0; j < currName.length(); j++) {
+                currScore += currName.charAt(j)-64;
+            }
+            total += currScore * (i + 1);
+            if(currName.equals("COLIN"))
+                System.out.printf("Found Colin; score is %d, spot is %d%n", currScore, i+1);
+        }
+        System.out.println(total);
     }
 
 }
