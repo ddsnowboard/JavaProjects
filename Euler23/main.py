5ce550de8f1e38f79c7f5abb430372215d30ecf9
ddsnowboard <ddsnowboard@gmail.com>
2015-01-12 17:02:37 -0600

    It finishes now, but it's wrong.

--- Euler23/main.py
+++ Euler23/main.py
@@ -1,4 +1,18 @@
 from math import sqrt
+def binarySearch(l, i):
+	if len(l) == 1:
+		if l == [i]:
+			return True
+		else:
+			return False
+	m = l[int(len(l)/2)]
+	if m == i:
+		return True
+	elif m > i:
+		return binarySearch(l[:int(len(l)/2)], i)
+	elif m < i:
+		return binarySearch(l[int(len(l)/2):], i)
+	return False
 def divisors(i):
 	out = []
 	for p in range(2, int(sqrt(i)+1)):
@@ -6,20 +20,23 @@ def divisors(i):
 			out.append(p)
 			out.append(i/p)
 	return out
-abundants = []
+abundants = {}
 for i in range(28123):
 	if sum(divisors(i)) > i:
-		abundants.append(i)
+		abundants[i] = True
 out = []
 counter = 0
 for i in range(28123):
-	counter += 1
 	if counter == 100:
 		counter = 0
 		print(i)
+	counter += 1
 	for p in abundants:
 		if p<i:
-			if (i-p) in abundants:
-				continue
+			try:
+				if abundants[i-p]:
+					continue
+			except KeyError:
+				pass
 	out.append(i)
 print(sum(out))
