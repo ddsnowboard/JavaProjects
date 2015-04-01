7bd3ded064af5212865bcadc64826c045bc20cee
ddsnowboard <ddsnowboard@gmail.com>
2015-04-01 12:31:11 -0500

    Getting closer.
    It does something. I don't know exactly what, but it is giving me a bigger, but wrong, number.

--- Euler47/src/Euler47.java
+++ Euler47/src/Euler47.java
@@ -1,36 +1,36 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Euler47 {
 	// https://projecteuler.net/problem=47
-	// I understand the question now. The thing they're asking is whether there are three numbers in a row that each 
-	// themselves have three distinct prime factors, or four with four. Ok. Got it. 
+	// I understand the question now. The thing they're asking is whether there
+	// are three numbers in a row that each
+	// themselves have three distinct prime factors, or four with four. Ok. Got
+	// it.
 	public static void main(String[] args) {
 		Factorizer factorizer = new Factorizer();
-		int currentFirstNumber = 2;
+		int currentFirstNumber = 1;
 		final int LENGTH_OF_SEQUENCE = 3;
 		int[] currentFour = new int[LENGTH_OF_SEQUENCE];
-		HashMap<Integer, Boolean> currentFactors = new HashMap<>();
+		ArrayList<HashSet<Integer>> currentFourFactors;
 		boolean found = false;
 		ArrayList<Integer> currentNumberFactors;
 		while (!found) {
+			currentFirstNumber++;
 			for (int i = 0; i < LENGTH_OF_SEQUENCE; i++)
 				currentFour[i] = currentFirstNumber + i;
-			for (int i : currentFour) {
-				currentNumberFactors = factorizer.primeFactors(i);
+			currentFourFactors = new ArrayList<>();
+			for (int i = 0; i < LENGTH_OF_SEQUENCE; i++) {
+				currentNumberFactors = factorizer.primeFactors(currentFour[i]);
+				currentFourFactors.add(new HashSet<>());
 				for (int j : currentNumberFactors) {
-					if (!currentFactors.containsKey(j)) {
-						currentFactors.put(j, true);
-						found = true;
-					} else {
-						found = false;
-						System.out.println("Overwritten!");
-						break;
-					}
+					currentFourFactors.get(i).add(j);
 				}
 			}
-			currentFirstNumber++;
-			currentFactors = new HashMap<Integer, Boolean>();
+			found = true;
+			for (HashSet<Integer> s : currentFourFactors) {
+				found = found && s.size() == LENGTH_OF_SEQUENCE;
+			}
 		}
 		System.out.println(currentFirstNumber);
 	}
