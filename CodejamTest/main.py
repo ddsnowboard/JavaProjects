1430b7dcf87f00df30e5d6c40956ece39f2b5c21
ddsnowboard <ddsnowboard@gmail.com>
2015-04-04 23:52:04 -0500

    Remove Codejam file.
    I moved it elsewhere.

--- CodejamTest/main.py
+++
@@ -1,6 +0,0 @@
-with open("B-large-practice.in") as f:
-    with open("output.txt", 'w') as w:
-        f.readline()
-        for i, j in enumerate(f):
-            j = j.strip()
-            w.write("Case #{}: {}\n".format(i+1, " ".join(j.split(" ")[::-1])))
