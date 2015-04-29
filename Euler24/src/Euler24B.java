e5f69deac4ed936b01c96d2c986a4983d7b9399a
ddsnowboard <ddsnowboard@gmail.com>
2015-04-29 10:04:25 -0500

    Euler24 should work, but it will be really slow.
    I need a way to check for some easy conditions first to make it faster.

--- Euler24/src/Euler24B.java
+++ Euler24/src/Euler24B.java
@@ -1,45 +1,45 @@
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Euler24B {
 	public static void main(String[] args) {
-		ArrayList<Double> numbers = new ArrayList<>();
-		final double GOAL = 5;
-		double currentNumber = 012;
-		int[] NUMBERS = { 0, 1, 2 };
-		// final double GOAL = 1e6;
-		// double currentNumber = 0123456789d;
-		// int[] NUMBERS = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		HashMap<Character, Boolean> currSet;
+		ArrayList<Double> foundNumbers = new ArrayList<>();
+		// final double GOAL = 6;
+		// double currentNumber = 012;
+		// Integer[] NUMBERS = { 0, 1, 2 };
+		final double GOAL = 1e6;
+		double currentNumber = 0123456789d;
+		final Integer[] NUMBERS = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		// This is the set of all the numbers that are present in the number we're currently 
+		// looking at. 
+		MyHashMap<Character, Boolean> currSet;
+		// This is the string representation of the number we're currently looking at. 
 		String currString;
-		DecimalFormat df = new DecimalFormat(repeatString("0", NUMBERS.length));
-		boolean good;
-		while (numbers.size() < GOAL) {
-			currSet = new HashMap<>();
-			currString = df.format(currentNumber);
+		// This is the thing that formats the numbers into strings like we want them. 
+		final DecimalFormat FORMAT = new DecimalFormat(repeatString("0", NUMBERS.length));
+		
+		while (foundNumbers.size() < GOAL) {
+			currSet = new MyHashMap<>();
+			currString = FORMAT.format(currentNumber);
 			for (int i = 0; i < currString.length(); i++) {
 				currSet.put(currString.charAt(i), Boolean.TRUE);
 			}
-			good = true;
-			for (int p = 0; p < currString.length(); p++) {
-
-			}
-			if (currSet.con() == NUMBERS.length) {
-				numbers.add(currentNumber);
-				if (numbers.size() % 1000 == 0)
-					System.out.printf("Found %d%n", numbers.size());
-				// System.out.println(currString);
+			if (currSet.findAllInKeys(Arrays.asList(NUMBERS).stream()
+					.map(x -> String.valueOf(x).charAt(0)).collect(Collectors.toList()))) {
+				foundNumbers.add(currentNumber);
+				if (foundNumbers.size() % 1000 == 0)
+					System.out.printf("Found %d%n", foundNumbers.size());
 			}
 			currentNumber++;
 		}
-		System.out.println(df.format(numbers.get(numbers.size() - 1)));
+		System.out.println(FORMAT.format(foundNumbers.get(foundNumbers.size() - 1)));
 	}
 
 	public static String repeatString(String s, int times) {
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < times; i++)
-		{
+		for (int i = 0; i < times; i++) {
 			sb.append(s);
 		}
 		return sb.toString();
--- Euler24/src/MyHashMap.java
+++ Euler24/src/MyHashMap.java
@@ -1,17 +1,21 @@
 import java.util.HashMap;
 import java.util.List;
 
-
-public class MyHashMap extends HashMap<K, V> {
+public class MyHashMap<K, V> extends HashMap<K, V> {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 962145404455470638L;
-	// What is the K right over there? I need to figure that out. And also write this. Just do a reduce thing, it'll be nice. 
-	public <K> boolean findAllInKeys(List<K> l)
-	{
-		
+
+	// What is the K right over there? I need to figure that out. And also write
+	// this. Just do a reduce thing, it'll be nice.
+	public boolean findAllInKeys(List<K> l) {
+		boolean out = true;
+		for (K i : l) {
+			out = out && this.containsKey(i);
+		}
+		return out;
 	}
 
 }
