dab8c5e364cbd4bfbdb28df87929ed62e756ab02
Will Koster <ddsnowboard@gmail.com>
2015-08-07 23:04:44 -0500

    The numbers are too big.
    Malloc is causing a segfault, but I can't figure out why. It apparently
    could be happening anywhere and would just show up with this. Maybe I
    should call it less.

--- Euler40/main.c
+++ Euler40/main.c
@@ -1,20 +1,35 @@
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include <stdio.h>
 // https://projecteuler.net/problem=40
 // #define LENGTH 1000000
-#define LENGTH 30
+#define LENGTH 300000
 
 int main(int argc, char** argv)
 {
-    char* number = malloc(LENGTH + 10);
+    char* number = malloc(LENGTH);
+    char* holder = malloc(LENGTH);
+    int currLength = LENGTH;
     char* currNumber;
-    int i = 0;
+    int i = 1;
     for(;i<LENGTH;i++)
     {
-        currNumber = malloc(5);
+        printf("We're on %d\n", i);
+        printf("mallocing %d\n", (int) (log(i) / log(10)) + 2);
+        currNumber = malloc(((int) log(i) / log(10)) + 1);
+        printf("Got past currnumber\n");
         sprintf(currNumber, "%d", i);
+        if(strlen(currNumber) + strlen(number) > currLength)
+        {
+            strcpy(holder, currNumber);
+            free(currNumber);
+            currNumber = malloc(currLength * 2);
+            currLength *= 2;
+            strcpy(currNumber, holder);
+        }
         strcat(number, currNumber);
+        free(currNumber);
     } 
     printf("%s\n", number);
     return 0;
