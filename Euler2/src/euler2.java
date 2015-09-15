3d39060d653c15cd4345be1bbe43715211905d16
Will Koster <ddsnowboard@gmail.com>
2015-09-14 19:34:32 -0500

    Change euler2
    It's better now. It was bad.

--- Euler2/src/euler2.java
+++ Euler2/src/euler2.java
@@ -1,30 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 public class euler2 {
-public euler2(){}
-static public void main(String[] args) {
-		boolean done=false;
-		int i=1;
-		int p = 0;
-		int q=0;
-		List<Integer> seq = new ArrayList<Integer>(0);
-		seq.add(1);
-		seq.add(2);
-		while(!done)
-		{
-			 p=seq.get(i);
-			 q=seq.get(i-1);
-			if(p+q<=4000000){
-				seq.add(p+q);
-			}
-			else 
-			{
-				done=true;
-			}
-			
-		}
-		System.out.println("ding!");
-	}
+    public euler2(){}
+    public static void main(String[] args) {
+        boolean done = false;
+        int i = 1;
+        int p = 0;
+        int q = 0;
+        List<Integer> seq = new ArrayList<Integer>();
+        seq.add(1);
+        seq.add(2);
+        while(!done)
+        {
+            p = seq.get(i);
+            q = seq.get(i-1);
+            if(p+q <= 4000000){
+                seq.add(p + q);
+            }
+            else 
+            {
+                done = true;
+            }
+            i++;
+        }
+        System.out.println("ding!");
+    }
 }
 
 
