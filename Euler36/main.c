c80ccc660cbe1ef3cd4e08f59445bfc927b6b302
Will Koster <ddsnowboard@gmail.com>
2016-02-05 08:07:31 -0600

    Move forward on Euler36
    Itoa can't be too much harder.

--- Euler36/main.c
+++ Euler36/main.c
@@ -8,17 +8,18 @@ char* itobin(int i);
 
 int main(int argc, char** argv) 
 {
-    int tot = 0;
-    char* currNum;
-    for(int i = 0; i <= 1000000; ++i)
-    {
-        currNum = malloc(10);
-        sprintf(currNum, "%d", i);
-        if(strcmp(currNum, reverse(currNum)) == 0)
-        {
-            tot += i;
-        }
-    }
+    printf("%s", itoa(350));
+    // int tot = 0;
+    // char* currNum;
+    // for(int i = 0; i <= 1000000; ++i)
+    // {
+    //     currNum = malloc(10);
+    //     sprintf(currNum, "%d", i);
+    //     if(strcmp(currNum, reverse(currNum)) == 0)
+    //     {
+    //         tot += i;
+    //     }
+    // }
 }
 char* reverse(char* s)
 {
@@ -31,4 +32,19 @@ char* reverse(char* s)
     }
     out[j + 1] = '\0';
     return out;
+}
+char* itoa(int i)
+{
+    int exp = 1;
+    int idx = 0;
+    char* out = malloc(30);
+    while(i % exp != i)
+    {
+        printf("exp is %d", exp);
+        out[idx] = (i % exp) + '0';
+        exp *= 10;
+        idx++;
+    }
+    out[idx] = '\0';
+    return reverse(out);
 }
