9e31068e05b5de356456b9bb4de54288f50a7e2a
Will Koster <ddsnowboard@gmail.com>
2016-02-11 17:29:00 -0600

    Finish Euler34
    Start on threading. I want this to work almost instantly. It's not
    working so well. Threads are complicated. I don't know why, but it's
    creating like 90,000 threads and then stopping. The number isn't staying
    constant though. So that's weird.

--- Euler34/main.c
+++ Euler34/main.c
@@ -2,20 +2,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <pthread.h>
+#define MAX 9999999 
 int numLen(int i);
 int* digits(int i);
+int fact(int i);
+void* test(void* s);
+struct Inputs {
+    int i;
+    int* tot;
+    int* counter;
+};
 
 int main(int argc, char** argv)
 {
     int i = 3;
-    int* digits;
-    for(;i < 1000; ++i)
+    int out = 0;
+    int counter = 0;
+    for(; i < MAX; ++i)
     {
-        
+        pthread_t thread;
+        struct Inputs input;
+        input.i = i;
+        input.tot = &out;
+        input.counter = &counter;
+        pthread_create(&thread, NULL, &test, (void *)&input);
     }
+    while(counter < MAX)
+    {
+        printf("counter is %d", counter);
+    }
+    printf("The sum is %d\n", out);
     return 0;
 }
 
+void* test(void* s)
+{
+    struct Inputs* inputs = (struct Inputs *)s;
+    int i = inputs -> i;
+    int* nums = digits(i);
+    printf("Entered thread with i = %d", i);
+    int len = numLen(i);
+    int c = 0;
+    int tot = 0;
+    for(; c < len;++c)
+    {
+        tot += fact(nums[c]);
+    }
+    if(tot == i)
+    {
+        *(inputs -> tot) += i;
+    }
+    (*(inputs -> counter))++;
+    return 0;
+}
 int numLen(int i)
 {
     double l = log10(i);
@@ -31,4 +71,13 @@ int* digits(int i)
         i /= 10;
     }
     return out;
+}
+int fact(int i)
+{
+    // It's ok that this will never work for big numbers because all its inputs will be 
+    // one digit. 
+    if(i == 1 || i == 0)
+        return 1;
+    else
+        return i * fact(i - 1);
 }
