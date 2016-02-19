964277dd70286096df18a21b7230f831ece4955c
Will Koster <ddsnowboard@gmail.com>
2016-02-19 01:41:29 -0600

    Make some progress
    My prime finding function is so thread unsafe, and very particular, but
    super fast! There is some other sort of issue though, and I don't know
    what it is.

--- Euler37/main.c
+++ Euler37/main.c
@@ -2,49 +2,88 @@
 #include <stdlib.h>
 #define MAX 10000
 /* Use a binary tree to memoize the primes, because why not? */
-struct TreeNode {
-    int value;
-    struct TreeNode *right;
-    struct TreeNode *left;
-};
 struct ListNode {
     int value;
     struct ListNode *next;
 };
-extern struct TreeNode head;
+struct ListNode primes;
 int isPrime(int i);
-int treeSearch(int i);
-int treeAdd(int i);
+int push(struct ListNode *list, int toAdd);
+int isTruncatablePrime(int i);
 
 int main(int argc, char** argv)
 {
+    // Add sample primes
+    struct ListNode *ptr = malloc(sizeof(struct ListNode));
+    primes = *ptr;
+    primes.value = 1;
+    primes.next = NULL;
+    push(&primes, 2);
+    push(&primes, 3);
+    push(&primes, 5);
+    push(&primes, 7);
     int tot = 0;
-    int i = 0;
+    int i = 10;
     for(; i < MAX; ++i)
     {
         if(isTruncatablePrime(i))
         {
-            printf("%d", i);
+            printf("%d\n", i);
             tot += i;
         }
-        printf("The sum is %d", i);
     }
+    printf("The sum is %d\n", i);
     return 0;
 }
 
 int isPrime(int i)
 {
+    // This function only works if it gets each number in order
     if(i % 2 == 0)
     {
         return 0;
     }
-    else if(treeSearch(i))
+    else
     {
-        return 1;
-    }
-    else 
-    {
-        struct TreeNode walker = head;
+        // Skip 1 and 2
+        struct ListNode walker = *(primes.next -> next);
+        while(walker.next && i != walker.value && i % walker.value != 0)
+            walker = *walker.next;
+        if(walker.next)
+        {
+            if(i == walker.value)
+                return 1;
+            else if(i % walker.value == 0)
+                return 0;
+        }
+        else
+        {
+            push(&primes, i);
+            return 1;
+        }
     }
     return 0;
+}
+int push(struct ListNode *list, int toAdd)
+{
+    struct ListNode *walker = list;
+    while(walker -> next)
+        walker = walker -> next;
+    struct ListNode *next = malloc(sizeof(struct ListNode));
+    next -> value = toAdd;
+    walker -> next = next;
+    return 0;
+}
+int isTruncatablePrime(int i)
+{
+    int orig = i;
+    int out = isPrime(i);
+    int counter = (int)10e9;
+    while(i % counter == i)
+        counter /= 10;
+    while((out & isPrime(i %= counter)) && i)
+        counter /= 10;
+    i = orig;
+    while((out & isPrime(i /= 10)) && i);
+    return out;
 }
