c8fae1e53acf107cffb0f39855a6c2d4c30436bc
Will Koster <ddsnowboard@gmail.com>
2016-02-24 19:24:57 -0600

    Edit siteGenerator.c slightly
    Refactor script to use native JavaScript, finally, and remove warnings.

--- PlayPrograms/siteGenerator.c
+++ PlayPrograms/siteGenerator.c
@@ -37,12 +37,12 @@ int main(void)
     char* TAB = "    ";
     FILE* in = fopen(CUE_LIST, "r");
     FILE* out = fopen(OUTPUT_FILE, "w");
-    char* SCRIPTS = "<html><head><style>*{font-family: monospace; } p { font-size: 1.4em;} .clickable{margin-top:0;margin-bottom:0}.clicked{background-color:#000000;color:#ffffff;}span{text-decoration:underline;}</style><script src=\"https://code.jquery.com/jquery-1.11.2.min.js\"></script><script>$(document).ready(function(){var last=null;$(\".clickable\").click(function(){$(this).toggleClass(\"clicked\");if(last!=null){last.toggleClass(\"clicked\");}; last=$(this);});$(document).keydown(function(event){if(event.which===37){last.toggleClass(\"clicked\");$(\"#\"+(parseInt(last.attr('id'),10)-1).toString()).toggleClass(\"clicked\");last=$(\"#\"+(parseInt(last.attr('id'),10)-1).toString());}else if(event.which===39){last.toggleClass(\"clicked\");$(\"#\"+(parseInt(last.attr('id'),10)+1).toString()).toggleClass(\"clicked\");last=$(\"#\"+(parseInt(last.attr('id'),10)+1).toString());} console.log(\"#\"+(parseInt(last.attr('id'),10)+1).toString());});});</script><ul>";
+    char *SCRIPTS = "<html><head><style>*{font-family: monospace; } p { font-size: 1.4em;} .clickable{margin-top:0;margin-bottom:0}.clicked{background-color:#000000;color:#ffffff;}span{text-decoration:underline;}</style> <script> function toggleClass(el, cls) { if(el.classList.contains(cls)) el.classList.remove(cls); else el.classList.add(cls); } window.onload = function(){ var last = null; var clickables = document.getElementsByClassName(\"clickable\"); for(var i = 0; i < clickables.length; i++) { clickables[i].addEventListener(\"click\", function() { toggleClass(this, \"clicked\"); if(last && this !== last) last.classList.remove(\"clicked\"); last = this; }); } document.onkeydown = function(e) { if(e.which === 37) { toggleClass(last, \"clicked\"); var el = document.getElementById(parseInt(last.getAttribute(\"id\")) - 1); toggleClass(el, \"clicked\"); last = el; } else if(e.which === 39) { toggleClass(last, \"clicked\"); var el = document.getElementById(parseInt(last.getAttribute(\"id\")) + 1); toggleClass(el, \"clicked\"); last = el; } }; }; </script>";
     fprintf(out, "%s", SCRIPTS);
     char* line = malloc(MAX_LINE_LENGTH);
     int currLine = 0;
     int depth = 0;
-    while(line = fgets(line, MAX_LINE_LENGTH, in))
+    while((line = fgets(line, MAX_LINE_LENGTH, in)))
     {
         int tabs = count(line, TAB);
         if(tabs > depth)
@@ -73,4 +73,5 @@ int main(void)
     }
     fclose(out);
     fclose(in);
+    return 0;
 }
