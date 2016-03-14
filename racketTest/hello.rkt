f514e9144b8163e1d28028eedfc3f3c7b92f907b
Will Koster <ddsnowboard@gmail.com>
2016-03-13 20:50:26 -0500

    Got it
    Racket is kinda hard. I'm sure I'll get the hang of it.

--- racketTest/hello.rkt
+++ racketTest/hello.rkt
@@ -1,10 +1,10 @@
 #lang racket
-(define (fizzbuzz n)
-  (define (_fizzbuzz i)
-    (if (= 0 (remainder i 15)) "fizzbuzz" 
-      (if (= 0 (remainder i 5)) "buzz" 
-        (if (= 0 (remainder i 3)) "buzz" i))))
-  (if (= n 1) 1 
-    (let ([i n])
-      (string-append (_fizzbuzz i) (fizzbuzz (- i 1)))))
-  (print (fizzbuzz 15))
+(define (fizzbuzz n l)
+  (define (otherFizz i)
+    (if (= 0 (remainder i 15)) "fizzbuzz\n" 
+        (if (= 0 (remainder i 5)) "buzz\n" 
+            (if (= 0 (remainder i 3)) "fizz\n" (string-append (number->string i) "\n")))))
+  (if (= n 0) l 
+      (fizzbuzz (- n 1) (cons (let ([out (otherFizz n)])
+                                 (if (number? out) (number->string out) out)) l))))
+(display (fizzbuzz 1000 '()))
