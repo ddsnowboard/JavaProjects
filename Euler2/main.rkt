78349a6ed3fb731dd8fd6d9a946ef99cd071afcb
Will Koster <ddsnowboard@gmail.com>
2016-03-14 14:40:11 -0500

    Try to make it work
    I didn't get very far

--- Euler2/main.rkt
+++ Euler2/main.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(define nextFib n l 
+  (if (= n 0) l 
+    ((cons (+ (list-ref (- (length l) 1)) (list-ref (- (length l) 1))) l))))
 (display (for/sum ([i (lambda (n) 
                         (let ([a 1] [b 2])
-
