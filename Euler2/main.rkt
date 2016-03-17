79b1cd5e830575577ca2aadfe8a479f44846c690
Will Koster <ddsnowboard@gmail.com>
2016-03-17 09:45:04 -0500

    Add a line to make testing easier
    Racket has a *ton* of overhead

--- Euler2/main.rkt
+++ Euler2/main.rkt
@@ -3,7 +3,9 @@
   (define (nextFib li)
     (+ (last li) (list-ref li (- (length li) 2))))
   (define next (nextFib l))
-  (if (<= next 2140000000)
+  (if 
+    ;(<= next 2140000000)
+    (<= next 45)
     (smallFibs (append l (list next)))
     l))
 (display (for/sum ([i (smallFibs '(1 2))] #:when (= (modulo i 2) 0)) i))
