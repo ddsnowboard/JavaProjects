acc48cfa1c17cff6c9fbf42d8f912bd79fd896a1
Will Koster <ddsnowboard@gmail.com>
2016-03-18 10:38:25 -0500

    Works
    It's slow as hell though. C works in a third of the time, without any
    fancy algorithms or anything. Where is all the time going?

--- Euler5/main.rkt
+++ Euler5/main.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (define (divisible? n)
   ; All the prime numbers between 1 and 20 will take care of all the other numbers
-  (if (for/and ([i '(2 3 5 7 11 13 17 19)]) (remainder n i))
+  (if (not (for/and ([i (in-range 1 21)]) (= (remainder n i) 0)))
     (divisible? (+ n 1))
     n))
 (display (divisible? 11))
