67e1aa7860c532ff80cc727162470f55de2542b5
Will Koster <ddsnowboard@gmail.com>
2016-03-18 10:49:09 -0500

    Remove comment

--- Euler5/main.rkt
+++ Euler5/main.rkt
@@ -1,6 +1,5 @@
 #lang racket
 (define (divisible? n)
-  ; All the prime numbers between 1 and 20 will take care of all the other numbers
   (if (not (for/and ([i (in-range 1 21)]) (= (remainder n i) 0)))
     (divisible? (+ n 1))
     n))
