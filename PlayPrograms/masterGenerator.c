a5a11cff41936661494489020d3c4ef4ee4cd577
Will Koster <ddsnowboard@gmail.com>
2016-03-19 14:18:20 -0500

    Start on masterGenerator.c
    It's going alright.

---
+++ PlayPrograms/masterGenerator.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LINE_SIZE 100
+// This is just for testing
+#define OUTPUT_FILE "MasterCueList123.txt"
+#define LS_COMMAND "ls -v1"
+#define LS_FILE ".output.txt"
+
+char* getLine(FILE *f);
+char* chompExt(char* filename);
+
+int main(int argc, char** argv)
+{
+    // I don't know how to get sh to ignore empty globs, so I'll just worry 
+    // about it later and do this for now.
+    // const char *AUDIO_FORMATS[] = {".wav", ".mp3"};
+    const char *AUDIO_FORMATS[] = {".mp3"};
+    const int AUDIO_FORMATS_LEN = 1;
+    char *command = (char *) malloc(strlen(LS_COMMAND) + (9 * AUDIO_FORMATS_LEN) + 35);
+    strcat(command, LS_COMMAND);
+    int i = 0;
+    for(; i < AUDIO_FORMATS_LEN; ++i)
+    {
+        strcat(command, " *");
+        strcat(command, AUDIO_FORMATS[i]);
+        // strcat(command, "(N)");
+    }
+    strcat(command, " > ");
+    strcat(command, LS_FILE);
+    system(command);
+    FILE *lsOutput = fopen(LS_FILE, "r");
+    if(lsOutput == NULL)
+    {
+        printf("Something bad happened!\n");
+        return 1;
+    }
+    // I think I can run through the lines and print them now. So that's nice.
+    // I'm going to have to replicate my fancy logic for subjugation or whatever
+    // though. Maybe...
+    char *output = getLine(lsOutput);
+    printf("output was %s\n", output);
+    remove(LS_FILE);
+    return 0;
+}
+
+char* getLine(FILE *f)
+{
+    int allocatedMemory = 32;
+    char *almostOut = (char *) malloc(allocatedMemory);
+    char *nextBite = (char *) malloc(2);
+    do
+    {
+        if(fread(nextBite, 1, 1, f) != 0)
+            strcat(almostOut, nextBite);
+        else
+            break;
+    } while(nextBite[0] != '\n');
+    // Sans newline, plus null terminator
+    size_t len = strlen(almostOut);
+    char *out = (char *) malloc(len - 1 + 1);
+    strncpy(out, almostOut, len - 1);
+    free(almostOut);
+    return out;
+}
+
+char* chompExt(char* filename)
+{
+    int i = strlen(filename) - 1;
+    for(; i > 0; --i)
+    {
+        if(filename[i] == '.')
+        {
+            char* out = (char *) malloc(i + 1);
+            strncpy(out, filename, i);
+            return out;
+        }
+    }
+    return filename;
+}
