00aeef9b51d5c6e69560f9cc37547b7881796245
Will Koster <ddsnowboard@gmail.com>
2016-03-19 21:10:24 -0500

    Finish most of masterGenerator.c
    I still need to add the stuff that automatically adds tabs, but I can do
    that tomorrow.

--- PlayPrograms/masterGenerator.c
+++ PlayPrograms/masterGenerator.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #define MAX_LINE_SIZE 100
 // This is just for testing
 #define OUTPUT_FILE "MasterCueList123.txt"
@@ -12,19 +14,37 @@ char* chompExt(char* filename);
 
 int main(int argc, char** argv)
 {
+    if(!(access(OUTPUT_FILE, F_OK) == -1 || (argc >= 2 && strcmp(argv[1], "-f") == 0)))
+    {
+        printf("Use -f to force a new file\n");
+        return 1;
+    }
     // I don't know how to get sh to ignore empty globs, so I'll just worry 
     // about it later and do this for now.
-    // const char *AUDIO_FORMATS[] = {".wav", ".mp3"};
-    const char *AUDIO_FORMATS[] = {".mp3"};
-    const int AUDIO_FORMATS_LEN = 1;
+    const char *AUDIO_FORMATS[] = {".wav", ".mp3"};
+    const int AUDIO_FORMATS_LEN = 2;
     char *command = (char *) malloc(strlen(LS_COMMAND) + (9 * AUDIO_FORMATS_LEN) + 35);
     strcat(command, LS_COMMAND);
     int i = 0;
+    int filesExist = 0;
     for(; i < AUDIO_FORMATS_LEN; ++i)
     {
-        strcat(command, " *");
-        strcat(command, AUDIO_FORMATS[i]);
-        // strcat(command, "(N)");
+        char *testCmd = (char *) malloc(15);
+        strcat(testCmd, "ls *");
+        strcat(testCmd, AUDIO_FORMATS[i]);
+        strcat(testCmd, " 2>/dev/null > /dev/null");
+        if(system(testCmd) == 0)
+        {
+            strcat(command, " *");
+            strcat(command, AUDIO_FORMATS[i]);
+            filesExist = 1;
+        }
+    }
+    if(filesExist != 1)
+    {
+        printf("There are no music files!\n");
+        free(command);
+        return 2;
     }
     strcat(command, " > ");
     strcat(command, LS_FILE);
@@ -33,13 +53,22 @@ int main(int argc, char** argv)
     if(lsOutput == NULL)
     {
         printf("Something bad happened!\n");
+        fclose(lsOutput);
+        remove(LS_FILE);
         return 1;
     }
     // I think I can run through the lines and print them now. So that's nice.
     // I'm going to have to replicate my fancy logic for subjugation or whatever
-    // though. Maybe...
-    char *output = getLine(lsOutput);
-    printf("output was %s\n", output);
+    // though. Or not...
+    char *output;
+    FILE *outFile = fopen(OUTPUT_FILE, "w");
+    while(strcmp(output = getLine(lsOutput), "") != 0)
+    {
+        fputs(chompExt(output), outFile);
+        fputs(":\n", outFile);
+    }
+    fclose(outFile);
+    fclose(lsOutput);
     remove(LS_FILE);
     return 0;
 }
@@ -58,7 +87,9 @@ char* getLine(FILE *f)
     } while(nextBite[0] != '\n');
     // Sans newline, plus null terminator
     size_t len = strlen(almostOut);
-    char *out = (char *) malloc(len - 1 + 1);
+    if(len == 0)
+        return "";
+    char *out = (char *) calloc(1, len - 1 + 1);
     strncpy(out, almostOut, len - 1);
     free(almostOut);
     return out;
