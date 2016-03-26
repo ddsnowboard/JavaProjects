c281ad3a43298e7a7beed4655a2a7510d35066a9
Will Koster <ddsnowboard@gmail.com>
2016-03-26 17:40:35 -0500

    Start on C version
    C is so much fun.

---
+++ Euler22/main.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define INPUT_FILE "input.txt"
+#define MAX_NAME_LENGTH 10
+
+struct Node {
+    char *val;
+    struct Node *next;
+};
+struct Node *head;
+
+void push(char *val);
+char* get(int idx);
+
+int main(int argc, char** argv)
+{
+    FILE* in = fopen(INPUT_FILE, "r");
+    char next;
+    char* currString = calloc(MAX_NAME_LENGTH, sizeof(char));
+    int currStrLen = 0;
+    while((next = fgetc(in)) != EOF)
+    {
+        if(next == '"')
+        {
+            continue;
+        }
+        else if(next == ',')
+        {
+            push(currString);
+            currString = calloc(MAX_NAME_LENGTH, sizeof(char));
+            currStrLen = 0;
+        }
+        else
+        {
+            currString[currStrLen++] = next;
+        }
+    }
+    return 0; 
+}
+
+void push(char *val)
+{
+    struct Node *curr = head;
+    struct Node new;
+    new.val = val;
+    new.next = NULL;
+    if(head == NULL)
+    {
+        printf("Head was null; reassigning to %s\n", val);
+        head = &new;
+        return;
+    }
+    while(curr->next != NULL)
+        curr = curr->next;
+    curr->next = &new;
+}
+
+char* get(int idx)
+{
+    struct Node *curr = head;
+    while(idx > 0)
+    {
+        idx--;
+        curr = curr->next;
+    }
+    return curr->val;
+}
