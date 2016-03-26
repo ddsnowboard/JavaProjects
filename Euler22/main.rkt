ce51d89f212781c84dc43dd49afc47fed34a13ba
Will Koster <ddsnowboard@gmail.com>
2016-03-26 11:28:50 -0500

    Racket version works
    It's really slow though

--- Euler22/main.rkt
+++ Euler22/main.rkt
@@ -3,8 +3,57 @@
 ; For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 ; 
 ; What is the total of all the name scores in the file?
-#lang racket/base
+#lang racket
+(define (index needle haystack)
+  (define (helper needle haystack n)
+    (if (>= n (string-length haystack))
+      -1
+      (if (eq? (string-ref haystack n) needle)
+        n
+        (helper needle haystack (+ 1 n)))))
+  (helper needle haystack 0))
+(define (score letter)
+  (define scores (hash "A" 1 
+                       "B" 2 
+                       "C" 3 
+                       "D" 4 
+                       "E" 5 
+                       "F" 6 
+                       "G" 7 
+                       "H" 8 
+                       "I" 9 
+                       "J" 10 
+                       "K" 11 
+                       "L" 12 
+                       "M" 13 
+                       "N" 14 
+                       "O" 15 
+                       "P" 16 
+                       "Q" 17 
+                       "R" 18 
+                       "S" 19 
+                       "T" 20 
+                       "U" 21 
+                       "V" 22 
+                       "W" 23 
+                       "X" 24 
+                       "Y" 25 
+                       "Z" 26))
+  (hash-ref scores (string letter)))
+
+(define (scoreWord str)
+  (for/sum ([j (string->list str)]) (score j)))
+
+(define (multiplyByPlace l)
+  (define (helper l n)
+    (if (null? (cdr l))
+      (cons (* (car l) n) '())
+      (cons (* (car l) n) (helper (cdr l) (+ 1 n)))))
+  (helper l 1))
+
 (define input (open-input-file "input.txt"))
 (define sNames (read-line input))
 (close-input-port input)
-(display sNames)
+(define lNames (sort (string-split (list->string (remove* '(#\") (string->list sNames))) ",") string<?))
+(define scores (multiplyByPlace (for/list ([i lNames]) (scoreWord i))))
+(display (for/sum ([i scores]) i))
