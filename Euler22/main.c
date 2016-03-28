08fa82b0c280821a956948b76580a96696de0e94
Will Koster <ddsnowboard@gmail.com>
2016-03-28 13:40:36 -0500

    Make some progress on Euler22.c
    I now need to write the linked list sorting algorithm. This is fun.

--- Euler22/main.c
+++ Euler22/main.c
@@ -1,17 +1,24 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #define INPUT_FILE "input.txt"
 #define MAX_NAME_LENGTH 10
 
 struct Node {
     char *val;
+    struct Node *prev;
     struct Node *next;
 };
 struct Node *head;
+struct Node *tail;
 
 void push(char *val);
 char* get(int idx);
 void printList();
+void swap(struct Node *a, struct Node *b);
+void swapIdx(int a, int b);
+int compare(struct Node *a, struct Node *b);
+void sort(struct Node *head, struct Node *tail);
 
 int main(int argc, char** argv)
 {
@@ -36,43 +43,102 @@ int main(int argc, char** argv)
             currString[currStrLen++] = next;
         }
     }
+    printList();
     return 0; 
 }
 
 void push(char *val)
 {
-    struct Node *curr = head;
     struct Node *new = (struct Node *) malloc(sizeof(struct Node));
     new->val = val;
     new->next = NULL;
     if(head == NULL)
     {
         printf("Head was null; reassigning to %s\n", val);
+        new->prev = NULL;
         head = new;
+        tail = new;
         return;
     }
-    while(curr->next != NULL)
-        curr = curr->next;
-    curr->next = new;
+    else{
+        new->prev = tail;
+        tail->next = new;
+        tail = new;
+    }
 }
 
 char* get(int idx)
 {
-    struct Node *curr = head;
-    while(idx > 0)
+    if(idx < 0)
     {
-        idx--;
-        curr = curr->next;
+        idx *= -1;
+        struct Node *curr = tail;
+        while(idx > 0)
+        {
+            idx--;
+            curr = curr->prev;
+        }
+        return curr->val;
     }
-    return curr->val;
+    else{
+        struct Node *curr = head;
+        while(idx > 0)
+        {
+            idx--;
+            curr = curr->next;
+        }
+        return curr->val;
+    }
 }
 
 void printList(void)
 {
+    printf("First, forward\n");
     struct Node *curr = head;
-    while(curr->next != NULL)
+    while((curr = curr->next) != NULL)
     {
         printf("%s\n", curr->val);
-        curr = curr->next;
     }
+    printf("\n\n\n\nNow, backward\n");
+    curr = tail;
+    while(curr->prev != NULL)
+    {
+        printf("%s\n", curr->val);
+        curr = curr->prev;
+    }
+}
+
+void swap(struct Node *a, struct Node *b)
+{
+    char* temp = a->val;
+    a->val = b->val;
+    b->val = temp;
+}
+
+void swapIdx(int a, int b)
+{
+    struct Node *nA = head;
+    struct Node *nB = head;
+
+    while(a > 0)
+    {
+        nA = nA->next;
+        a--;
+    }
+    while(b > 0)
+    {
+        nB = nB->next;
+        b--;
+    }
+    swap(nA, nB);
+}
+
+int compare(struct Node *a, struct Node *b)
+{
+    return strcmp(a->val, b->val);
+}
+
+void sort(struct Node *head, struct Node *tail)
+{
+
 }
