5cd6057729276cf3f5a47c61cd1cb63be2d388f0
Will Koster <ddsnowboard@gmail.com>
2016-04-09 13:34:01 -0500

    Fix 1-17
    I think my getLine() was broken. It's good now though, because I
    replaced it with a library function. Who knew that that existed!

--- KandR/1-17.c
+++ KandR/1-17.c
@@ -4,33 +4,21 @@
 #include <string.h>
 
 // Write a program to print all input lines that are longer than 80 characters
-char* getLine(FILE *f);
 
 int main(int argc, char** argv)
 {
     FILE *f = argc == 1 ? stdin : fopen(argv[1], "r");
+    size_t size = 0;
+    ssize_t ret;
     char *curr;
-    while(strcmp(curr = getLine(f), "") != 0)
+    curr = NULL;
+    getline(&curr, 0, f);
+    while((ret = getline(&curr, &size, f)) != -1)
     {
         if(strlen(curr) > 80)
             printf("%s", curr);
         free(curr);
+        curr = NULL;
     }
     return 0;
-}
-
-char* getLine(FILE *f)
-{
-    char *line = malloc(MAXLINE);
-    int idx = 0;
-    char curr = fgetc(f);
-    if(curr == EOF)
-    {
-        free(line);
-        return "";
-    }
-    do
-        line[idx++] = curr;
-    while((curr = fgetc(f)) != EOF && curr != '\n');
-    return line;
 }
