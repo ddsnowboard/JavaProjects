ec796d52709be4717f9c2a6950bc8cff611da572
Will Koster <ddsnowboard@gmail.com>
2016-04-11 21:16:13 -0500

    Start 1-20

---
+++ KandR/1-20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Write a program `detab` that replaces tab stops in the input 
+// with the proper number of blanks to space to the next tab stop. Assume a fixed set of tab stops,
+// say every *n* columns.
+
+int main(int argc, char **argv)
+{
+    return 0; 
+}
