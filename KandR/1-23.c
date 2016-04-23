7a89b55e7a8c630faef7cd72d56363ac0b51d28a
Will Koster <ddsnowboard@gmail.com>
2016-04-23 10:24:23 -0500

    Start on 1-23
    It seems to be going well so far.

---
+++ KandR/1-23.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Write a program to remove all comments from a C program. Don't forget to handle quoted strings
+// and character constants properly. C comments do not nest.
+
+int main(int argc, char **argv)
+{
+    char *line = NULL;
+    size_t length = 0;
+    while(getline(&line, &length, stdin) != -1)
+    {
+        int i = 0;
+        char curr = line[i];
+        char lastChar = '\0';
+        int commentable = 1;
+        char beginningDelimiter;
+        do{
+            if(curr == '/' && lastChar == '/' && commentable == 1)
+            {
+                line[i] = '\0';
+                break;
+            }
+            else if(curr == '\'' || curr == '"')
+            {
+                if(!(lastChar == '\\'))
+                {
+                    if(commentable)
+                    {
+                        commentable = 0;
+                        beginningDelimiter = curr;
+                    }
+                    else
+                    {
+                        if(beginningDelimiter == curr)
+                            commentable = 1;
+                    }
+                }
+            }
+            lastChar = curr;
+        } while((curr = line[++i]));
+        printf("%s\n", line);
+    }
+    return 0;
+}
