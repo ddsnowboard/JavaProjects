573f14b5e8eb755a724492fe095b42b5437599ba
Will Koster <ddsnowboard@gmail.com>
2016-04-29 10:04:12 -0500

    Add some test code
    It works! So far, at least.

--- KandR/1-24.c
+++ KandR/1-24.c
@@ -5,5 +5,9 @@
 int main(int argc, char **argv)
 {
     struct ht_Table *table = ht_create(5);
+    ht_put(table, 't', 5);
+    printf("%d\n", *ht_get(table, 't'));
+    ht_put(table, 'y', 6);
+    printf("%d\n", *ht_get(table, 'y'));
     return 0;
 }
