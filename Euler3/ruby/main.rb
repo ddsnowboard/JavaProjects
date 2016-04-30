56c56bcc80309f484b57b38119beeceae61fb5a0
Will Koster <ddsnowboard@gmail.com>
2016-04-30 13:23:52 -0500

    Finish Euler3
    Ruby is actually pretty fast. It beat my (old) Jave implementation

--- Euler3/ruby/main.rb
+++ Euler3/ruby/main.rb
@@ -1,3 +1,49 @@
-class Primerator
-    @primes = []
+class PrimeMachine
+    @@primes = [2, 3, 5, 7]
+    def initialize
+        @index = 0
+    end
+    def next_prime
+        if @@primes[@index]
+            @index += 1
+            return @@primes[@index - 1]
+        else
+            next_prime = @@primes[-1] + 2
+            while not is_prime next_prime
+                next_prime += 1
+            end
+            @@primes.push(next_prime)
+            @index += 1
+            return next_prime
+        end
+    end
+
+    private
+    def is_prime(n)
+        # You better have already generated all the prime numbers
+        # below this one, or else this function won't work.
+        for i in @@primes
+            if n % i == 0
+                return false
+            elsif i > (n / 2)
+                return true
+            end
+        end
+        return true
+    end
 end
+
+def factorize(n)
+    pm = PrimeMachine.new
+    np = pm.next_prime
+    until n % np == 0 or np > (n / 2)
+        np = pm.next_prime
+    end
+    if np > n / 2
+        return [n]
+    else
+        return [np] + factorize(n / np)
+    end
+end
+
+puts(factorize(600851475143).max)
