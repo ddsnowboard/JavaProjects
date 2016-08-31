ff08205a663a5a75a09f7066be0c2da32429c32e
Will Koster <jameswk2@illinois.edu>
2016-08-31 18:45:47 -0500

    Write some init functions for treeToLinkedList
    Also write some convenience functions.

--- Interview/treeToDLinkedList.c
+++ Interview/treeToDLinkedList.c
@@ -12,7 +12,63 @@ struct Node {
     struct Node *right;
 };
 
+struct Node *initializeTree();
+struct Node *getNodePtr();
+void printLinkedList(struct Node head);
 int main(int argc, char** argv)
 {
+    struct Node *head = initializeTree();
+    // This could be harder than I thought
     return 0;
+}
+
+struct Node *initializeTree()
+{
+    struct Node *out;
+    struct Node *currLeft = getNodePtr();
+    struct Node *currRight = getNodePtr();
+    struct Node *currHead = getNodePtr();
+    currLeft->value = 4;
+    currLeft->left = NULL;
+    currLeft->right = NULL;
+    currRight->value = 8;
+    currRight->left = NULL;
+    currRight->right = NULL;
+    currHead->value = 6;
+    currHead->left = currLeft;
+    currHead->right = currRight;
+    currLeft = currHead;
+    currHead = getNodePtr();
+    currHead->value = 10;
+    currHead->left = currLeft;
+    out = currHead;
+    currRight = getNodePtr();
+    currRight->value = 14;
+    currHead->right = currRight;
+    currHead = currRight;
+    currRight = getNodePtr();
+    currLeft = getNodePtr();
+    currRight->value = 16;
+    currLeft->value = 12;
+    currLeft->right = NULL;
+    currLeft->left = NULL;
+    currRight->right = NULL;
+    currRight->left = NULL;
+    currHead->left = currLeft;
+    currHead->right = currRight;
+    return out;
+}
+
+struct Node *getNodePtr()
+{
+     return (struct Node*) malloc(sizeof(struct Node));
+}
+
+void printLinkedList(struct Node head)
+{
+    while(head->right != NULL)
+    {
+        printf("%d ", head->value);
+        head = head->right;
+    }
 }
