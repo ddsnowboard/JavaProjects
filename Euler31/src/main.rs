5ddba16dbbb91e78e95921488a6f60b35d792fae
Will Koster <jameswk2@illinois.edu>
2016-12-23 15:53:06 -0600

    Make some progress

--- Euler31/src/main.rs
+++ Euler31/src/main.rs
@@ -1,10 +1,7 @@
-// This might mostly be for nought, but we'll see. So my current plan is to use a linked list.
-// We'll make a branch for each different denomination we use, and we'll add the heads to a big
-// set at the end. Then we'll count the set, and that'll be the answer.
 #![allow(non_snake_case)]
 use std::collections::HashSet;
 
-#[derive(Hash, Eq, PartialEq)]
+#[derive(Hash, Eq, PartialEq, Clone, Copy)]
 struct Link<'a, T: 'a>
 {
     value: T,
@@ -31,39 +28,46 @@ fn main() {
 
 fn count_ways<'a> (amount: i32, denominations: &[i32]) -> i32 {
 
-    let mut set : HashSet<&'a Link<i32>> = HashSet::new();
+    let mut set : HashSet<Link<i32>> = HashSet::new();
     for denomination in denominations {
         _count_ways(amount, denominations, *denomination, &mut set, None);
     }
     set.len() as i32
 }
 
-fn _count_ways<'a> (amount: i32, denominations: &[i32], addedCoin: i32, list: &mut HashSet<&'a Link<'a, i32>>, prevNode: Option<&'a Link<'a, i32>>)
+fn _count_ways<'a> (amount: i32, denominations: &[i32], addedCoin: i32, list: &mut HashSet<Link<'a, i32>>, prevNode: Option<Link<'a, i32>>)
 {
     if amount == 0 {
         match prevNode {
-            Some(x) => {list.insert(x);},
+            Some(x) => {list.insert(x.clone());},
             None => {panic!("You must have called the function with 0!")},
         }
     }
     else if amount < 0 {
         return;
     }
-    let newNode = Link<'a> {
+    let otherX = match prevNode {
+            Some(x) => Some(& x.clone()),
+            None => None};
+
+    // I don't understand lifetimes well enough to make 
+    // this work. It says that I can't put the next node into this newNode 
+    // because the next node doesn't live long enough. I don't know how to fix it. 
+    let newNode = Link {
         value: addedCoin,
-        next: prevNode,
+        next: otherX, 
     };
     let newAmount = amount - addedCoin;
     for denom in denominations {
-        _count_ways(newAmount, denominations, *denom, list, Some(& newNode));
+        _count_ways(newAmount, denominations, *denom, list, Some(newNode.clone()));
     }
 }
 
 
 #[test]
 fn testLinkedList() {
-    let mut link3 = Link{value: 56, next: None};
-    let mut link2 = Link{ value: 53, next: None};
+    let mut link3 = Link {value: 56, next: None};
+    let mut link2 = Link {value: 53, next: None};
     let mut link = Link {value: 55, next: None};
     assert_eq!(link.length(), 1);
     link2.next = Some(& link3);
