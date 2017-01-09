a37225197d9ed512c546a5321165da14ed1016d5
Will Koster <jameswk2@illinois.edu>
2017-01-09 12:22:22 -0600

    Do Euler31 in Rust
    Rust is pretty nice. Damn it has good error messages.

--- Euler31/src/main.rs
+++ Euler31/src/main.rs
@@ -1,22 +1,4 @@
 #![allow(non_snake_case)]
-use std::collections::HashSet;
-
-#[derive(Hash, Eq, PartialEq, Clone, Copy)]
-struct Link<'a, T: 'a>
-{
-    value: T,
-    next: Option<&'a Link<'a, T>>,
-}
-
-impl<'a, T: 'a> Link<'a, T> {
-    fn length(&self) -> usize {
-        match self.next {
-            Some(x) => 1 + x.length(),
-            None => 1, 
-        }
-    }
-}
-
 
 fn main() {
     let denominations = [1, 2, 5, 10, 20, 50, 100, 200];
@@ -27,43 +9,13 @@ fn main() {
 
 
 fn count_ways(amount: i32, denominations: &[i32]) -> i32 {
-
-    let mut set : HashSet<Link<i32>> = HashSet::new();
-    for denomination in denominations {
-        _count_ways(amount, denominations, *denomination, &mut set, Link {value: *denomination, next: None});
+    let mut ways = vec![0; (amount + 1) as usize];
+    ways[0] = 1;
+    for &denom in denominations {
+        for num in denom..amount + 1 {
+            ways[num as usize] += ways[(num - denom) as usize];
+        }
     }
-    set.len() as i32
-}
-
-fn _count_ways<'a>(amount: i32, denominations: &[i32], addedCoin: i32, list: &mut HashSet<Link<'a, i32>>, prevNode: Link<'a, i32>) {
-    if amount == 0 {
-        list.insert(prevNode.clone());
-        return;
-    }
-    else if amount < 0 {
-        return;
-    }
-
-    let newNode = Link {
-        value: addedCoin,
-        next: Some(&prevNode), 
-    };
-
-    let newAmount = amount - addedCoin;
-    for denom in denominations {
-        _count_ways(newAmount, denominations, *denom, list, newNode);
-    }
-}
-
-
-#[test]
-fn testLinkedList() {
-    let mut link3 = Link {value: 56, next: None};
-    let mut link2 = Link {value: 53, next: None};
-    let mut link = Link {value: 55, next: None};
-    assert_eq!(link.length(), 1);
-    link2.next = Some(& link3);
-    link.next = Some(& link2);
-    assert_eq!(link.length(), 3);
+    ways[amount as usize]
 }
 
