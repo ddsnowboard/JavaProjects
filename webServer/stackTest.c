b42d3c729c18ddf94c2c41245f40a2d6b7999fac
Will Koster <jameswk2@illinois.edu>
2017-02-28 12:16:51 -0600

    Write test for stack

--- webServer/stackTest.c
+++ webServer/stackTest.c
@@ -1,6 +1,27 @@
+#include <stdio.h>
 #include "stack.h"
 
 int main(int argc, char** argv) {
     struct stack* s = stack_create();
+    stack_push(s, 'a');
+    stack_push(s, 'p');
+    stack_push(s, 'p');
+    stack_push(s, 'l');
+    stack_push(s, 'e');
+    while(!stack_empty(s)) 
+        printf("%c", stack_pop(s));
+    printf("\n");
+    stack_free(s);
+    s = stack_create();
+    int i;
+    for(i = 0; i < 1000; i++)
+    {
+        stack_push(s, 'a');
+    }
+    int counter = 0;
+    while(!stack_empty(s)) {
+        printf("Popped off a %c, at count %d\n", stack_pop(s), counter++);
+    }
+    stack_free(s);
     return 0;
 }
