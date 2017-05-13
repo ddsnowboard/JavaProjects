0565ddf09d81419dd9f1ce7f4c2fa61b1681e48c
Will Koster <jameswk2@illinois.edu>
2017-05-13 10:32:30 -0500

    Start on Euler18
    Nominally

---
+++ Euler18/main.cs
@@ -0,0 +1,14 @@
+// https://projecteuler.net/problem=18
+using System;
+
+public class Hello {
+    static public void Main() {
+        int CurrentLineCount = 0;
+        int CurrentLineLength = 1;
+        for(var i = 0; i < 100; i++) {
+            // I'm trying to print out a pyramid just like the one on the site but that just ascends so 
+            // I can get a good data structure for this.
+            Console.Write ("
+        }
+    }
+}
