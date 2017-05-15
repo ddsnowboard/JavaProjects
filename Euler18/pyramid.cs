94d70ec109d03902af022dd85ff7abeb28ea0403
Will Koster <jameswk2@illinois.edu>
2017-05-14 20:47:04 -0500

    Finish
    That was fun. And my algorithm was really good. So apparently school is
    actually pretty helpful.

--- Euler18/pyramid.cs
+++ Euler18/pyramid.cs
@@ -6,16 +6,78 @@
 using System.Collections.Generic;
 using System;
 
-public class Pyramid {
+public class Pyramid 
+{
+    public int Count 
+    {
+        get 
+        { 
+            return arr.Count; 
+        }
+    }
+
     private List<int> arr = new List<int>();
 
-    public Pyramid(string input) {
-        string[] strings = input.Split(new string[] {" "}, StringSplitOptions.None);
+    public int this[int index] 
+    {
+        get 
+        {
+            return arr[index];
+        }
+    }
+
+    public Pyramid(string input) 
+    {
+        string[] strings = input.Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
         Array.ForEach(strings, delegate(string s) {
-                arr.Add(Int32.Parse(s));
+                Add(Int32.Parse(s));
                 });
     }
 
-    // I'm going to get the index using a dict, so you can pass in a number and it will know the index and work properly
-    // for left and right child
+    public void Add(int toAdd) 
+    {
+        arr.Add(toAdd);
+    }
+
+    // You are going to have to know the index and the number. That's life. I can probably override 
+    // the [] operator which will make it better though.
+
+    public int GetLeftChild(int index) 
+    {
+        int row = GetRow(index);
+        int offset = GetOffset(index);
+        int rowStart = index - offset;
+        int retval = rowStart + row + offset + 1;
+        if(retval >= Count)
+        {
+            return -1;
+        }
+        return retval;
+    }
+
+    public int GetRightChild(int index) 
+    {
+        int retval = GetLeftChild(index) + 1;
+        if(retval >= Count)
+        {
+            return -1;
+        }
+        return retval;
+    }
+
+    /*
+     * Note that this is zero-indexed
+     */
+    private int GetRow(int index) 
+    {
+        int DoubleIndex = index * 2;
+        double row = ((-1.0 + Math.Sqrt(1 + 4 * DoubleIndex)) / 2.0);
+        return (int) row;
+    }
+
+    private int GetOffset(int index) 
+    {
+        int row = GetRow(index);
+        return index - ((row  * (row + 1)) / 2);
+    }
 }
