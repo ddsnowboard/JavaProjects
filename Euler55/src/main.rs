5c28cf7be4c4f6c56f0fff7feb2f7c2158789574
Will Koster <jameswk2@illinois.edu>
2017-07-07 20:20:56 -0500

    Try to change everything to BigInts
    This is always a disaster

--- Euler55/src/main.rs
+++ Euler55/src/main.rs
@@ -1,27 +1,30 @@
 use std::collections::VecDeque;
+extern crate num;
+use num::bigint::BigUint;
+use num::FromPrimitive;
 
 fn main() {
     let amount = (1..10000).fold(0, |acc, n| if is_lychrel(n) { acc + 1 } else { acc });
     println!("Amount is {}", amount);
 }
 
-fn is_lychrel(mut n: u64) -> bool {
-    const MAX_ITERATIONS: u64 = 50;
+fn is_lychrel(mut n: u32) -> bool {
+    const MAX_ITERATIONS: u8 = 50;
     let mut counter = MAX_ITERATIONS;
-    let mut current_number = n;
+    let mut current_number = BigUint::from_u32(n).unwrap();
     while counter > 0 {
-        let reverse = reverse_number(n);
-        println!("Adding {} and {}", n, reverse);
-        n += reverse;
+        let reverse = reverse_number(current_number);
+        println!("Adding {} and {}", current_number, reverse);
+        current_number = current_number + reverse;
         counter -= 1;
-        if n == reverse_number(n) {
+        if current_number == reverse_number(current_number) {
             return false;
         }
     }
     true
 }
 
-fn reverse_number(mut n: u64) -> u64 {
+fn reverse_number(mut n: BigUint) -> BigUint {
     let mut digits = VecDeque::new();
     while n > 0 {
         digits.push_front(n % 10);
