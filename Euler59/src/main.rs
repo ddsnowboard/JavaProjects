847c599eae8e7632bc0d094c3306d028d7ac4f11
Will Koster <jameswk2@illinois.edu>
2017-07-10 22:27:42 -0500

    Almost do Euler59 in Rust
    It's so slow. I'm not sure why. It's something with string checking? I
    dunno. Maybe gcc was using AVX instructions or something. That's
    possible, actually.

---
+++ Euler59/src/main.rs
@@ -0,0 +1,108 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::char;
+use std::u32;
+use std::convert::From;
+use std::io::BufReader;
+
+const KEY_LENGTH: usize = 3;
+const LONGEST_WORDS_IN_DICTIONARY: usize = 28;
+fn main() {
+    let mut contents = String::new();
+    const FILENAME: &'static str = "cipher.txt";
+    {
+        let mut file = File::open(FILENAME).unwrap();
+        file.read_to_string(&mut contents).unwrap();
+    }
+    let ascii_values = to_vec_of_ints(&contents);
+
+    let dict = Dictionary::new();
+
+    let mut max = 0;
+    let mut max_key = String::from("");
+    for key in KeyIterator::new(KEY_LENGTH) {
+        let decrypted = decrypt(&ascii_values, &key);
+        let words: Vec<&str> = decrypted.split_whitespace().collect();
+        if words[0].len() <= LONGEST_WORDS_IN_DICTIONARY {
+            let count = dict.union(&words);
+            if count > max {
+                max = count;
+                max_key = String::from(format!("{:?}", key));
+            }
+        }
+    }
+    println!("Most was {} with {}", max, max_key);
+}
+
+fn to_vec_of_ints(string: &str) -> Vec<u32> {
+    string.split(",").map(|x| x.trim())
+        .map(|x| x.parse().unwrap())
+        .collect()
+}
+
+fn decrypt(characters: &[u32], key: &[char]) -> String {
+    let cycler = key.into_iter().map(|x| u32::from(*x)).collect::<Vec<u32>>().into_iter().cycle();
+    characters.into_iter().zip(cycler)
+        .map(|(c, k)| *c ^ k)
+        .map(|x| std::char::from_u32(x).unwrap())
+        .collect()
+}
+
+struct KeyIterator {
+    current_password: Vec<char>, 
+    password_length: usize,
+}
+
+impl KeyIterator {
+    fn new(i: usize) -> KeyIterator {
+        KeyIterator {current_password: vec!['a'; i], password_length: i}
+    }
+}
+
+impl Iterator for KeyIterator {
+    type Item = Vec<char>;
+
+    fn next(&mut self) -> Option<Vec<char>> {
+        for idx in (0..self.password_length).rev() {
+            if self.current_password[idx] == 'z' {
+                self.current_password[idx] = 'a';
+                continue;
+            }
+            else {
+                self.current_password[idx] = std::char::from_u32(u32::from(self.current_password[idx]) + 1).unwrap();
+                return Some(self.current_password.clone());
+            }
+        }
+        None
+    }
+}
+
+struct Dictionary {
+    words: Vec<String>
+}
+
+impl Dictionary {
+    fn new() -> Dictionary {
+        const FILE_PATH: &'static str = "/usr/share/dict/words";
+        let file = File::open(FILE_PATH).unwrap();
+        let reader = BufReader::new(&file);
+        Dictionary { words: reader.lines().map(|x| x.unwrap()).collect() } 
+    }
+
+    fn union(&self, other_words: &[&str]) -> u32 {
+        const WORDS_TO_CHECK: usize = 5;
+        let mut counter = 0;
+        for other_words in other_words.into_iter().take(WORDS_TO_CHECK) {
+            let word = other_words.trim().to_lowercase();
+            for line in &self.words {
+                let line = String::from(line.trim());
+                if line == word {
+                    counter += 1;
+                    break;
+                }
+            }
+        }
+        counter
+    }
+}
+
