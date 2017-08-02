9756b9eb279573ee81d59bf08e37d0ff62bb95c0
Will Koster <jameswk2@illinois.edu>
2017-08-02 13:46:25 -0500

    make some small improvements to xkcd_parser
    I understand Rust better than I did before.

--- rustExperiments/xkcd_parser/src/main.rs
+++ rustExperiments/xkcd_parser/src/main.rs
@@ -10,14 +10,15 @@ use htmlescape::decode_html;
 fn main() {
     let mut easy = Easy::new();
     easy.follow_location(true).unwrap();
-    let arguments = env::args().collect::<Vec<String>>();
+    let arguments: Vec<String>> = env::args().collect();
     if arguments.len() == 1 {
         easy.url("http://xkcd.com").unwrap();
     }
     else {
-        let comic_number : u32 = arguments[1].trim().parse().expect("You have to give the comic number as the input");
+        let comic_number: u32 = arguments[1].trim().parse().expect("You have to give the comic number as the input");
         easy.url(format!("http://xkcd.com/{}", comic_number).as_ref()).unwrap();
     }
+
     let mut text = Vec::new();
     {
         let mut transfer = easy.transfer();
@@ -27,7 +28,9 @@ fn main() {
         }).unwrap();
         transfer.perform().unwrap();
     }
-    let s: String = text.iter().map(|c| match std::char::from_u32(*c as u32){ Some('\n') => ' ', Some(c) => c, None=> ' '}).collect();
+    let s: String = text.iter()
+        .map(|c| match std::char::from_u32(*c as u32){ Some('\n') => ' ', Some(c) => c, None=> ' '})
+        .collect();
 
     let r = Regex::new("<img src=\"//imgs\\.xkcd\\.com/comics/.*?\" title=\"(.*?)\"").unwrap();
     for cap in r.captures_iter(&s)
