7dcd765d927e5705811bdc68e74ffdd50697507a
Will Koster <wkoster@clearent.com>
2017-08-08 15:48:04 -0500

    Try to improve Euler59 more
    We'll see if it worked

--- Euler59/src/main.rs
+++ Euler59/src/main.rs
@@ -22,28 +22,33 @@ fn main() {
     let dict = Dictionary::new();
 
     let keys: Vec<Vec<char>> = KeyIterator::new(KEY_LENGTH).collect();
-    if let Some(Some((biggest_key, biggest_count))) = keys.par_iter().map(| key | {
+
+    let max_pair = keys.par_iter().map(| key | {
         let decrypted = decrypt(&ascii_values, &key);
         let words: Vec<&str> = decrypted.split_whitespace().collect();
-        if (&words).into_iter().all(|x| x.len() <= LONGEST_WORDS_IN_DICTIONARY) {
+        if words.iter().all(|x| x.len() <= LONGEST_WORDS_IN_DICTIONARY) {
             let count = dict.union(&words);
             Some((key.clone(), count))
         }
         else {
             None
         }
-    }).max_by_key(| ref pair | {
-        match **pair {
+    }).max_by_key(| &pair | {
+        match pair {
             Some((_, c)) => c,
             None => 0,
         }
-    })
+    });
+
+    // The outer Option is from max_by_key(), the inner one is from the lambda I gave map()
+    if let Some(Some((biggest_key, biggest_count))) = max_pair
     {
-        let key_as_string: String = biggest_key.iter().cloned().collect();
+        // Does this need iter or into_iter? Why do I need cloned()?
+        let key_as_string: String = biggest_key.cloned().iter().collect();
         println!("Most was {} with {}", biggest_count, key_as_string);
         let real_message = decrypt(&ascii_values, &biggest_key);
         println!("Message is \n{}", real_message);
-        println!("Sum is {}", real_message.as_bytes().into_iter().map(|x| *x as u32).sum::<u32>());
+        println!("Sum is {}", real_message.as_bytes().iter().map(|x| *x as u32).sum::<u32>());
     }
     else {
         println!("We didn't find anything");
@@ -57,8 +62,8 @@ fn to_vec_of_ints(string: &str) -> Vec<u32> {
 }
 
 fn decrypt(characters: &[u32], key: &[char]) -> String {
-    let cycler = key.into_iter().map(|x| u32::from(*x)).cycle();
-    characters.into_iter().zip(cycler)
+    let cycler = key.iter().map(|x| u32::from(*x)).cycle();
+    characters.iter().zip(cycler)
         .map(|(c, k)| *c ^ k)
         .map(|x| std::char::from_u32(x).unwrap())
         .collect()
@@ -78,6 +83,7 @@ impl KeyIterator {
 impl Iterator for KeyIterator {
     type Item = Vec<char>;
 
+    // Why doesn't this return a slice or a borrow?
     fn next(&mut self) -> Option<Vec<char>> {
         for idx in (0..self.password_length).rev() {
             if self.current_password[idx] == 'z' {
@@ -94,7 +100,7 @@ impl Iterator for KeyIterator {
 }
 
 struct Dictionary {
-    words: HashMap<String, bool>;
+    words: HashSet<String>;
 }
 
 impl Dictionary {
@@ -102,23 +108,19 @@ impl Dictionary {
         const FILE_PATH: &'static str = "/usr/share/dict/words";
         let file = File::open(FILE_PATH).unwrap();
         let reader = BufReader::new(&file);
-        Dictionary { words: reader.lines().map(|x| (String::from(x.unwrap()), true)).collect() } 
+        Dictionary { words: reader.lines().map(|x| String::from(x.unwrap()) ).collect() } 
     }
 
     fn contains(&self, word: &str) -> bool {
-        let word = other_word.trim().to_lowercase();
+        let word = word.trim().to_lowercase();
         self.words.contains(word)
     }
 
     fn union(&self, other_words: &[&str]) -> u32 {
         const WORDS_TO_CHECK: usize = 20;
-        let mut counter = 0;
-        for other_word in other_word.into_iter().take(WORDS_TO_CHECK) {
-            if(self.contains(other_word)) {
-                counter += 1;
-            }
-        }
-        counter
+        other_word.iter().take(WORDS_TO_CHECK)
+            .map(|word| match self.contains(word) { true => 1, false => 0 })
+            .sum()
     }
 }
 
