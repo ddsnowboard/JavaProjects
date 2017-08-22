76936e1831725fddc1ac86207bc0b9b7a952c4d7
Will Koster <jameswk2@illinois.edu>
2017-08-22 17:25:42 -0500

    Try to speed up Euler36
    I just stumbled on it and it was slow. But the Python version is really
    slow and I don't know why. It's really weird. And whenever I try to do
    fancy stuff it just gets worse. I guess it's just down to calling C as
    much as possible and Python is slow. That's a little frustrating though.

--- Euler36/main.py
+++ Euler36/main.py
@@ -1,5 +1,18 @@
-out = 0
-for i in range(1, 1000000):
-    if str(i) == str(i)[::-1] and "{0:b}".format(i) == "{0:b}".format(i)[::-1]:
-        out += i
-print(out)
+import cProfile
+
+from math import log10 as log, floor
+def is_palindrome_bin(i):
+    s = "{0:b}".format(i)
+    return s == s[::-1]
+
+def is_palindrome_dec(i):
+    s = str(i)
+    return s == s[::-1]
+
+def is_palindrome(i):
+    return is_palindrome_dec(i) and is_palindrome_bin(i)
+
+def main():
+    print(sum(i for i in range(1, 1000000) if is_palindrome(i)))
+
+main()
