82a9b1bc211a726a53484b9104ada40007f697bf
Will Koster <jameswk2@illinois.edu>
2017-08-30 15:33:40 -0500

    Make a lot of progress on Euler152
    I can't be that far now, but it's not working. I have a sneaking
    suspicion that it's working but just really slow, but I'm not sure how
    to fix that. So it goes. This is fun though. Debugging with the
    interpreter is pretty excellent.

--- Euler152/euler152.hs
+++ Euler152/euler152.hs
@@ -1,23 +1,34 @@
 import Numeric
 import Data.Char
 
-top = 80
+top = 5
 
 bottom = 2
 
-inverseSquares = [1 `div` x^2 | x <- [bottom..top]]
-
-inInverseSquares x = x `elem` inverseSquares
+inverseSquares = [1.0 / (fromIntegral x)^2 :: Float | x <- [bottom..top]]
 
 toBinString n = (showIntAtBase 2 intToDigit n "")
 
-isSet (_, "1") = True
-isSet (_, "0") = False
+isSet (i, '1') = i
+isSet (i, '0') = 0
 
-combination n = [x | x <- zip inverseSquares (toBinString n), isSet x]
+combination n = map isSet [x | x <- zip inverseSquares (toBinString n)]
 
-# Check that the length is greater than or equal to 2 and less than or equal to 80, 
-# then you should have all the numbers to turn into bit strings
-bitstrings = [x | x <- [0..], log(x) / log(2)
+bitLength :: Int -> Integer
+bitLength n = (floor(((log(fromIntegral n) :: Float) / (log(2.0) :: Float)) + 1)) :: Integer
 
-combinations = map combination bitstrings
+bitstrings :: Integer -> Integer -> [Int]
+bitstrings minLen maxLen =  [x | x <- (takeWhile (\x -> bitLength x <= maxLen) [0..]), (bitLength x) >= minLen]
+
+combinations = map combination (bitstrings bottom top)
+
+sums :: [Float]
+sums = map sum combinations
+
+delta = 0.1
+half = 0.5
+nearHalf :: Float -> Bool
+nearHalf x = (abs x - half) < delta
+
+main = do 
+    print (length [x | x <- sums, nearHalf x])
