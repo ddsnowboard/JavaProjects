0d8c19af6327fa304012f52d5244f49b858330a2
Will Koster <jameswk2@illinois.edu>
2017-09-17 14:37:19 -0500

    Improve Euler6
    Streams are so fun

--- Euler6/java/src/Euler6.java
+++ Euler6/java/src/Euler6.java
@@ -1,19 +1,9 @@
+import java.util.stream.IntStream;
 
 public class Euler6 {
-	public static void main(String[] args)
-	{
-		int sumSquares = 0;
-		for(int i = 1; i<=100;i++)
-		{
-			sumSquares+=i*i;
-		}
-		int squareSum = 0;
-		for(int i = 1;i<=100;i++)
-		{
-			squareSum+=i;
-		}
-		squareSum = squareSum*squareSum;
-		System.out.println(squareSum-sumSquares);
-	}
+    public static void main(String[] args)
+    {
+        System.out.println(IntStream.rangeClosed(1, 100).map(x -> x * x).sum() - Math.pow(IntStream.rangeClosed(1, 100).sum(), 2));
+    }
 
 }
