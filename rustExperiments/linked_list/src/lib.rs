fc4497c26078fd46ecaabe4ca0188ffc6cac19d5
Will Koster <jameswk2@illinois.edu>
2017-12-12 13:15:32 -0600

    Finish linked list
    There's a lot more to these data structures than I thought

--- rustExperiments/linked_list/src/lib.rs
+++ rustExperiments/linked_list/src/lib.rs
@@ -1,51 +1,86 @@
 mod linked_list {
     pub struct List<T> {
-        head: Box<Link<T>>
-    }
-
-    enum Link<T> {
-        Empty, 
-        More(Box<Node<T>>)
+        head: Link<T>
     }
 
     struct Node<T> {
         value: T, 
-        next: Box<Link<T>>
+        next: Link<T>
     }
+
+    type Link<T> = Option<Box<Node<T>>>;
 
     impl<T> List<T> {
         pub fn new() -> Self {
-            List {head: Box::new(Link::Empty)}
+            List {head: None}
         }
 
-        pub fn push_front(mut self, val: T) -> Self {
-            let oldHead = self.head;
-            let newNode = Node { value: val, next: oldHead };
-            let newHead = Box::new(Link::More(Box::new(newNode)));
-            self.head = newHead;
-            self
+        pub fn push_front(&mut self, val: T) {
+            use std::mem;
+            let newNode = Box::new(Node {
+                    value: val,
+                    next: self.head.take()
+                    });
+            self.head = Some(newNode);
         }
         
         pub fn pop_back(&mut self) -> Option<T> {
-            match *self.head {
-                Link::Empty => None,
-                Link::More(ref mut node) => List::remove_back(&mut *node)
-            }
+            use std::mem;
+            self.head.take().map(|node| {
+                    let node = *node;
+                    self.head = node.next;
+                    node.value
+                    })
         }
 
-        fn remove_back(node: &mut Node<T>) -> Option<T> {
-            None
-            /*
-            match *node.next {
-                Empty =>
-            }
-            */
+        pub fn peek(&self) -> Option<&T> {
+            self.head.as_ref().map(|node| {
+                    &node.value
+                    })
+        }
+
+        pub fn peek_mut(&mut self) -> Option<&mut T> {
+            self.head.as_mut().map(|node| {
+                    &mut node.value
+                    })
         }
     }
 
+}
+
+#[cfg(test)]
+mod test {
+    use super::linked_list::List;
     #[test]
-    fn it_works() {
-        let l = List::new();
-        assert!(*l.head == Link::Empty);
+    fn basic() {
+        let mut l = List::new();
+        l.push_front("apples");
+        l.push_front("are");
+        l.push_front("fun");
+        l.push_front("To");
+        l.push_front("Eat");
+
+        assert_eq!(l.pop_back(), Some("Eat"));
+        assert_eq!(l.peek(), Some(&"To"));
+        assert_eq!(l.pop_back(), Some("To"));
+        assert_eq!(l.pop_back(), Some("fun"));
+        assert_eq!(l.pop_back(), Some("are"));
+
+        l.push_front("rocks");
+        assert_eq!(l.peek(), Some(&"rocks"));
+        assert_eq!(l.pop_back(), Some("rocks"));
+
+        assert_eq!(l.pop_back(), Some("apples"));
+    }
+
+    #[test]
+    fn mutable_peek() {
+        let mut l = List::new();
+        l.push_front("apples".to_owned());
+        {
+            let apples: &mut String = l.peek_mut().unwrap();
+            apples.push_str(" are fun");
+        }
+        assert_eq!(l.pop_back(), Some("apples are fun".to_owned()));
     }
 }
