5b7e7adb36e51b7f2ceb9f3b365bc2e495d62819
Will Koster <jameswk2@illinois.edu>
2018-07-01 10:19:54 -0500

    Finish haskell rpn
    That was fun. There's something really elegant about Haskell.

--- HaskellRPN/rpn.hs
+++ HaskellRPN/rpn.hs
@@ -1,26 +1,31 @@
 import Data.Maybe
 main = do 
     input <- getLine
-    putStrLn $ show (evalRpn input)
+    let output = evalRpn input
+    case output of
+        Just n -> putStrLn $ (show n)
+        Nothing -> putStrLn "You made an error somewhere"
+    
 
 data Token = Add | Subtract | Multiply | Divide | Number Double
             
 
-evalRpn :: String -> Double
+evalRpn :: String -> Maybe Double
 evalRpn input = let 
     tokenStrings = words input
-    tokens = map tokenize tokens
+    tokens = map tokenize tokenStrings
     in
     eval tokens []
+    
 
 eval :: [Token] -> [Double] -> Maybe Double
 eval [] (x:xs) = Just x
 eval (Add:xs) (a:b:rest) = eval xs (a+b:rest)
-eval (Subtract:xs) (a:b:rest) = eval xs (a-b:rest)
+eval (Subtract:xs) (a:b:rest) = eval xs (b-a:rest)
 eval (Multiply:xs) (a:b:rest) = eval xs (a * b:rest)
-eval (Divide:xs) (a:b:rest) = eval xs (a / b:rest)
+eval (Divide:xs) (a:b:rest) = eval xs (b/a:rest)
 eval (Number n:xs) stack = eval xs (n:stack)
-eval [] [] = 0
+eval [] [] = Just 0
 eval _ _ = Nothing
 
 tokenize :: String -> Token
