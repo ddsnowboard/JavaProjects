433dc994e2cb2c09a58a84bdf47b47e5ad24e24c
Will Koster <jameswk2@illinois.edu>
2018-08-04 16:13:44 -0500

    Make progress on Euler56
    I might need a more complicated system than the one I have. It takes too
    much memory.

--- Euler56/main.hs
+++ Euler56/main.hs
@@ -1,9 +1,19 @@
-main = print findMaxSum
+main = print $ show findMaxSum
 
 findMaxSum :: Int
-findMaxSum = max (map sumDigits [a^b | a <- [1..100], b <- [1..100]])
+findMaxSum = maximum (map sumDigits [a^b | a <- [1..100], b <- [1..100]])
 
-sumDigits :: (Num a) a -> Int
+sumDigits :: (Integral a) => a -> a
 sumDigits n = sum (getDigits n)
 
-getDigits :: (Num a) a -> [Int]
+getDigits :: (Integral b) => b -> [b]
+getDigits n = getDigits' n []
+
+getDigits' :: (Integral c) => c -> [c] -> [c]
+getDigits' 0 l = l
+getDigits' n l = 
+            let 
+                divided = n `div` 10
+                onesDigit = n `mod` 10
+             in
+                getDigits' divided (onesDigit:l)
