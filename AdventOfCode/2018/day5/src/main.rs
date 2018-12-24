d33058b11c9d8121a681d749f21b0b48fa596b63
Will Koster <jameswk2@illinois.edu>
2018-12-23 19:22:00 -0600

    Add multithreading to day5
    In another episode of Concurrency Makes Things Worse, concurrency made
    things worse

--- AdventOfCode/2018/day5/src/main.rs
+++ AdventOfCode/2018/day5/src/main.rs
@@ -3,26 +3,42 @@ extern crate num_cpus;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::mem;
+use std::sync::Arc;
 use std::thread;
 
 fn main() {
-    let input = parse_input();
+    let input = Arc::new(parse_input());
+    let input = Arc::clone(&input);
     let mut distinct_characters: HashSet<char> = HashSet::new();
-    for c in input.chars() {
+    for c in input.chars().filter(|c| c.is_uppercase()) {
         distinct_characters.insert(c);
     }
     let mut distinct_characters: Vec<char> = distinct_characters.into_iter().collect();
     let cpus = num_cpus::get();
-    let block_size = distinct_characters.len() / cpus;
+    let block_size = (distinct_characters.len() as f64 / (cpus as f64)).ceil() as usize;
     let mut even_groups: Vec<Vec<char>> = Vec::new();
     for _ in 0..cpus {
-        even_groups.push(distinct_characters.split_off(block_size));
+        let split_spot = if block_size > distinct_characters.len() {
+            distinct_characters.len()
+        } else {
+            block_size
+        };
+        let mut next = distinct_characters.split_off(split_spot);
+        mem::swap(&mut next, &mut distinct_characters);
+        even_groups.push(next);
     }
-    if let Some(min) = even_groups
-        .iter()
+    assert!(even_groups.len() == cpus);
+    for l in even_groups.iter().map(|l| l.len()) {
+        println!("{}", l);
+    }
+    let handles: Vec<_> = even_groups
+        .into_iter()
         .map(|cs| {
-            thread::spawn(|| {
-                cs.iter()
+            let input = Arc::clone(&input);
+            thread::spawn(move || {
+                let min = cs
+                    .iter()
                     .map(|c| {
                         reduce_all(
                             &input
@@ -32,10 +48,16 @@ fn main() {
                         )
                         .len()
                     })
-                    .min()
+                    .min();
+                min
             })
         })
+        .collect();
+
+    if let Some(min) = handles
+        .into_iter()
         .map(|t| t.join().unwrap())
+        .filter_map(|x| if let Some(min) = x { Some(min) } else { None })
         .min()
     {
         println!("{}", min);
