e3c7b470f7a299e3a24bed81cfa317bce57f5bef
Will Koster <jameswk2@illinois.edu>
2020-05-11 22:16:00 -0500

    Fiddle more
    Now the code is how it was always supposed to be

--- medioid_voter/src/main.rs
+++ medioid_voter/src/main.rs
@@ -28,22 +28,18 @@ fn voter_is_closer(voter: Strategy, me: Strategy, other_dist: f64) -> bool {
     distance(voter, me) < other_dist
 }
 
-fn best_strategies(s1: Strategy, s2: Strategy) -> (Strategy, Strategy) {
-    fn best_strategy(other: Strategy) -> Strategy {
-        let distances: Arc<Vec<_>> =
-            Arc::new(VOTERS.par_iter().map(|v| distance(*v, other)).collect());
-        *STRATEGIES
-            .par_iter()
-            .max_by_key(|s| {
-                VOTERS
-                    .iter()
-                    .zip(distances.iter())
-                    .filter(|(v, distance_to_other)| voter_is_closer(**v, **s, **distance_to_other))
-                    .count()
-            })
-            .unwrap()
-    }
-    (best_strategy(s2), best_strategy(s1))
+fn best_strategy(other: Strategy) -> Strategy {
+    let distances: Arc<Vec<_>> = Arc::new(VOTERS.par_iter().map(|v| distance(*v, other)).collect());
+    *STRATEGIES
+        .par_iter()
+        .max_by_key(|s| {
+            VOTERS
+                .iter()
+                .zip(distances.iter())
+                .filter(|(v, distance_to_other)| voter_is_closer(**v, **s, **distance_to_other))
+                .count()
+        })
+        .unwrap()
 }
 
 fn simulate_iteration() {
@@ -53,11 +49,11 @@ fn simulate_iteration() {
     let (mut s1, mut s2) = (*start_strategies[0], *start_strategies[1]);
     loop {
         println!("{:?}", (s1, s2));
-        let new_strategies = best_strategies(s1, s2);
-        if new_strategies == (s1, s2) {
+        let new_s1 = best_strategy(s2);
+        let new_s2 = best_strategy(new_s1);
+        if (new_s1, new_s2) == (s1, s2) {
             break;
         }
-        let (new_s1, new_s2) = new_strategies;
         s1 = new_s1;
         s2 = new_s2;
     }
