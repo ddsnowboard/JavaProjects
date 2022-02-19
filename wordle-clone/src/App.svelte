49e4fe2a9de8063cbe9b9d709c4f7e7fcc68d247
Will Koster <jameswk2@illinois.edu>
2022-02-19 00:29:21 -0600

    Start on wordle clone

---
+++ wordle-clone/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Keyboard from './Keyboard.svelte';
+    import LetterBoard from './LetterBoard.svelte';
+
+    let correctLetters = ["C"];
+    let wrongLetters = ["W"];
+    let misplacedLetters = ["M"];
+
+    const processLetter = (event) => {
+        let letter = event.detail;
+        correctLetters = [...correctLetters, letter];
+    };
+
+</script>
+
+<LetterBoard correctWord="rocks" guesses={[correctLetters,wrongLetters,misplacedLetters]} />
+<Keyboard {correctLetters} {wrongLetters} {misplacedLetters} on:key={processLetter}/>
---
+++ wordle-clone/src/LetterBoard.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let correctWord;
+    export let guesses;
+    export let rows = 6;
+    export let columns = 5;
+
+    $: rowStrings = Array.from(Array(rows).keys()).map((row) => {
+        return Array.from(Array(columns).keys()).map((col) => {
+            if(row < guesses.length && col < guesses[row].length) {
+                return guesses[row][col]
+            } else {
+                return " "
+            }
+        }).join("");
+    });
+</script>
+
+
+{#each rowStrings as row}
+    <div class="row">
+    {#each row as letter, idx}
+        <div class="letterBox">{letter}</div>
+    {/each}
+    </div>
+{/each}
+
+<style>
+    .row div {
+        background-color: lightgray;
+        display: inline-block;
+        width: 2.8ch;
+        height: 2.8ch;
+        padding: 0.25em;
+        margin: 0.25em;
+        border-radius: 10%;
+    }
+
+    .row {
+        text-align: center;
+        display: flex;
+        justify-content: center;
+    }
+</style>
