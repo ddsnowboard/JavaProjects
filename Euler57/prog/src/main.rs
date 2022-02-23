f6dcde8d89fe0bf78366ab29ee793cc34809dcfc
Will Koster <jameswk2@illinois.edu>
2022-02-22 23:48:41 -0600

    Make everything work
    The numbers are too big. Hmm.

--- Euler57/prog/src/main.rs
+++ Euler57/prog/src/main.rs
@@ -1,9 +1,13 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::cmp::Ord;
 use std::cmp::Ordering;
 use std::cmp::PartialOrd;
-use std::cmp::Ord;
+use std::hash::Hash;
+use std::hash::Hasher;
 use std::ops::Add;
+use std::ops::Div;
+use std::ops::Mul;
 use std::ops::Neg;
 use std::ops::Sub;
 
@@ -13,19 +17,67 @@ lazy_static! {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let fs = (1..=8).map(|depth| {
+        (
+            (Fraction::from_string("1").unwrap() + sqrt_expansion_minus_1(depth)).reduce(),
+            depth,
+        )
+    });
+    for (f, depth) in fs {
+        println!("At depth {} we got {:?}", depth, f);
+    }
 }
 
-type Int = u32;
+fn sqrt_expansion_minus_1(depth: usize) -> Fraction {
+    match depth {
+        1 => Fraction::new(1, 2),
+        _ => (Fraction::new(1, 1) / (Fraction::new(2, 1) + sqrt_expansion_minus_1(depth - 1)))
+            .reduce(),
+    }
+}
 
-#[derive(Hash, PartialEq, Eq, Debug)]
+type Int = u128;
+
+#[derive(Debug, Clone, Copy)]
 struct Fraction {
     numerator: Int,
     denominator: Int,
     sign: bool,
 }
 
+impl PartialEq for Fraction {
+    fn eq(&self, other: &Self) -> bool {
+        let reduced_self = self.reduce();
+        let reduced_other = other.reduce();
+        reduced_self.numerator == reduced_other.numerator
+            && reduced_self.denominator == reduced_other.denominator
+            && reduced_self.sign == reduced_other.sign
+    }
+}
+
+impl Eq for Fraction {}
+
+impl Hash for Fraction {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let reduced = self.reduce();
+        reduced.numerator.hash(state);
+        reduced.denominator.hash(state);
+        reduced.sign.hash(state);
+    }
+}
+
 impl Fraction {
+    fn new(numerator: Int, denominator: Int) -> Self {
+        if denominator == 0 {
+            panic!("Can't divide by zero!");
+        }
+        Self {
+            numerator,
+            denominator,
+            sign: true, // if you want negative just use -
+        }
+    }
+
     fn reduce(&self) -> Self {
         let gcd = Self::gcd(self.numerator, self.denominator);
         Self {
@@ -48,6 +100,12 @@ impl Fraction {
         } else {
             Self::gcd(b, r)
         }
+    }
+
+    fn debug_eq(&self, other: &Self) -> bool {
+        self.numerator == other.numerator
+            && self.denominator == other.denominator
+            && self.sign == other.sign
     }
 
     pub fn from_string(s: &str) -> Result<Self, FractionParseError> {
@@ -118,7 +176,7 @@ impl PartialOrd for Fraction {
 }
 
 impl Ord for Fraction {
-    fn cmp(&self,other:&Self)->Ordering {
+    fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(other).unwrap()
     }
 }
@@ -135,7 +193,8 @@ impl Add for Fraction {
                 numerator: (self.numerator * other.denominator)
                     + (other.numerator * self.denominator),
                 denominator: self.denominator * other.denominator,
-            },
+            }
+            .reduce(),
         }
     }
 }
@@ -149,7 +208,7 @@ impl Sub for Fraction {
         }
         match (self.sign, other.sign) {
             (true, false) => self + -other,
-            (false, true) => -self + other,
+            (false, true) => -(-self + other),
             (s1, _) => Self {
                 sign: s1,
                 numerator: (self.numerator * other.denominator)
@@ -169,6 +228,28 @@ impl Neg for Fraction {
             denominator: self.denominator,
             sign: !self.sign,
         }
+    }
+}
+
+impl Mul for Fraction {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self {
+        let numerator = self.numerator * rhs.numerator;
+        let denominator = self.denominator * rhs.denominator;
+        Self {
+            numerator,
+            denominator,
+            sign: !(self.sign ^ rhs.sign),
+        }
+    }
+}
+
+impl Div for Fraction {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self {
+        self * rhs.reciprocal()
     }
 }
 
@@ -193,6 +274,14 @@ fn gcd_works_normally() {
     assert_eq!(Fraction::gcd(2260, 816), 4);
 
     assert_eq!(Fraction::gcd(30502, 188), 2);
+}
+
+#[test]
+fn lcm_works() {
+    let tests = vec![(4, 6, 12), (48, 180, 720)];
+    for (a, b, out) in &tests {
+        assert_eq!(Fraction::lcm(*a, *b), *out);
+    }
 }
 
 #[test]
@@ -261,12 +350,71 @@ fn fails_to_parse_bad_string() {
 fn test_adding_boring() {
     let f = Fraction::from_string("1/5").unwrap();
     let f2 = Fraction::from_string("2/5").unwrap();
-    assert_eq!(f+f2,Fraction::from_string("3/5").unwrap());
+    assert_eq!(f + f2, Fraction::from_string("3/5").unwrap());
+}
+
+#[test]
+fn test_adding_different_signs() {
+    let f = Fraction::from_string("-1/5").unwrap();
+    let f2 = Fraction::from_string("2/5").unwrap();
+    assert_eq!(f + f2, Fraction::from_string("1/5").unwrap());
+
+    let f = Fraction::from_string("1/5").unwrap();
+    let f2 = Fraction::from_string("-2/5").unwrap();
+    assert_eq!(f + f2, Fraction::from_string("-1/5").unwrap());
 }
 
 #[test]
 fn test_adding_reduces() {
     let f = Fraction::from_string("1/3").unwrap();
     let f2 = Fraction::from_string("2/3").unwrap();
-    assert_eq!(f+f2,Fraction::from_string("1").unwrap());
+    assert!((f + f2).debug_eq(&Fraction::from_string("1").unwrap()));
+}
+
+#[test]
+fn test_normal_subtraction() {
+    let f = Fraction::from_string("6/5").unwrap();
+    let f2 = Fraction::from_string("4/5").unwrap();
+    assert_eq!(f - f2, Fraction::from_string("2/5").unwrap());
+}
+
+#[test]
+fn test_different_sign_subtraction() {
+    let f = Fraction::from_string("6/5").unwrap();
+    let f2 = Fraction::from_string("-4/5").unwrap();
+    assert_eq!(f - f2, Fraction::from_string("2").unwrap());
+
+    let f = Fraction::from_string("-6/5").unwrap();
+    let f2 = Fraction::from_string("4/5").unwrap();
+    assert_eq!(f - f2, Fraction::from_string("-2").unwrap());
+}
+
+#[test]
+fn test_comparison() {
+    assert!(Fraction::new(1, 5) == Fraction::new(2, 10));
+    assert!(Fraction::new(1, 5) == Fraction::new(1, 5));
+    assert!(Fraction::new(2, 5) != Fraction::new(1, 5));
+
+    assert!(Fraction::new(2, 5) > Fraction::new(2, 10));
+    assert!(Fraction::new(1, 10) < Fraction::new(1, 5));
+}
+
+#[test]
+fn test_multiply() {
+    assert_eq!(
+        Fraction::new(5, 3) * Fraction::new(2, 5),
+        Fraction::new(10, 15)
+    );
+    assert_eq!(
+        Fraction::new(5, 3) * Fraction::new(2, 5),
+        Fraction::new(2, 3)
+    );
+}
+
+#[test]
+fn test_divide() {
+    assert_eq!(
+        Fraction::new(5, 3) / Fraction::new(5, 2),
+        Fraction::new(10, 15)
+    );
 }
