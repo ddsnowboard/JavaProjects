944cd613c1669f067f666c4020c7067e21aceedb
Will Koster <jameswk2@illinois.edu>
2024-01-16 23:28:26 -0600

    Implement mmap
    That's a lot faster. Huh.
    Also implemented a way better algorithm for finding the start and end of
    chunks. That's surely also helping.

--- billion_row_challenge/src/lib.rs
+++ billion_row_challenge/src/lib.rs
@@ -1,13 +1,9 @@
 use fxhash::FxHashMap;
 use memchr::memchr;
+use memmap::Mmap;
 use rayon::prelude::*;
 use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
 use std::io::BufWriter;
-use std::io::Read;
-use std::io::Seek;
-use std::io::SeekFrom;
 use std::io::Write;
 
 const FILENAME: &str = "measurements.txt";
@@ -55,108 +51,53 @@ impl City {
     }
 }
 
-fn get_cities(mut reader: impl BufRead) -> Vec<(String, City)> {
+fn get_cities(file: &[u8]) -> Vec<(String, City)> {
     let mut cities = FxHashMap::default();
-    let mut row_holder = String::with_capacity(128);
-    loop {
-        row_holder.clear();
-        match reader.read_line(&mut row_holder).unwrap() {
-            0 => break,
-            _ => {
-                let row = read_row(&row_holder);
-                let city = cities.entry(row.city).or_insert_with(City::new);
-                city.process_temp(row.temp);
+    let mut current_index = 0;
+    while current_index < file.len() {
+        let next_line = {
+            match memchr(b'\n', &file[current_index..]) {
+                Some(newline_idx) => &file[current_index..(current_index + newline_idx)],
+                _ => &file[current_index..],
             }
-        }
+        };
+        current_index += next_line.len() + 1;
+        let row = read_row(std::str::from_utf8(next_line).unwrap());
+        let city = cities.entry(row.city).or_insert_with(City::new);
+        city.process_temp(row.temp);
     }
     let mut cities: Vec<_> = cities.into_iter().collect();
     cities.sort_unstable_by_key(|(c, _)| c.clone());
     cities
 }
 
-fn split_up_file(
-    mut file: File,
-    chunk_index: u32,
-    total_chunks: u32,
-) -> FilePortion<BufReader<File>> {
-    // Instead of getting the lines first and then finding the offsets we want, let's just guess
-    // some offsets and then find the nearest lines. We can use mmap. That could work.
-    file.rewind().unwrap();
-    let n_lines = {
-        let file = file.try_clone().unwrap();
-        let mut reader = BufReader::new(file);
-        let mut out: u32 = 0;
-        loop {
-            let buf = reader.fill_buf().unwrap();
-            if buf.is_empty() {
-                break;
-            }
-            for c in buf {
-                if *c == b'\n' {
-                    out += 1;
+fn split_up_file(file: &[u8], n_threads: usize) -> Vec<&[u8]> {
+    let naive_block_size = file.len() / n_threads;
+    let naive_block_starts = (0..n_threads).map(|n| n * naive_block_size);
+    let actual_block_starts: Vec<_> = naive_block_starts
+        .map(|start| {
+            let mut start = start;
+            if start == file.len() - 1 {
+                panic!("This should never happen!")
+            } else {
+                while start > 0 && file[start - 1] != b'\n' {
+                    start -= 1
                 }
+                Some(start)
             }
-            let bytes_processed = buf.len();
-            reader.consume(bytes_processed);
-        }
-        out
-    };
+        })
+        // This is so that the last window ends in None
+        .chain(std::iter::once(None))
+        .collect();
 
-    let expected_lines_per_chunk = n_lines / total_chunks;
-    let lines_for_this_chunk = if chunk_index == total_chunks - 1 {
-        expected_lines_per_chunk + (n_lines % total_chunks)
-    } else {
-        expected_lines_per_chunk
-    };
-    let min_line_index = chunk_index * expected_lines_per_chunk;
-    let last_line_index = min_line_index + lines_for_this_chunk;
-    let (start_offset, end_offset) = {
-        file.rewind().unwrap();
-        let file = file.try_clone().unwrap();
-        let offsets: Vec<(u64, u64)> = line_offsets(file)
-            .skip(min_line_index as usize)
-            .take((last_line_index - min_line_index) as usize)
-            .collect();
-        (offsets[0].0, offsets.last().unwrap().1)
-    };
-    file.seek(SeekFrom::Start(start_offset)).unwrap();
-    FilePortion::new(BufReader::new(file), end_offset - start_offset)
-}
-
-fn line_offsets(file: File) -> Box<dyn Iterator<Item = (u64, u64)>> {
-    let mut reader = BufReader::new(file);
-    let mut next_start_offset: u64 = 0;
-    Box::new(std::iter::from_fn(move || {
-        let start = next_start_offset;
-        if let Some(n_bytes) = bytes_until(&mut reader, b'\n') {
-            let end_offset = start + (n_bytes as u64);
-            next_start_offset = end_offset;
-            Some((start, end_offset))
-        } else {
-            None
-        }
-    }))
-}
-
-/// Stolen from the BufRead code
-fn bytes_until<R: BufRead + ?Sized>(r: &mut R, delim: u8) -> Option<usize> {
-    let mut read = 0;
-    loop {
-        let (done, used) = {
-            let available = r.fill_buf().unwrap();
-            match memchr(delim, available) {
-                Some(i) => (true, i + 1),
-                None => (false, available.len()),
-            }
-        };
-        r.consume(used);
-        read += used;
-        if done {
-            return Some(read);
-        } else if used == 0 {
-            return None;
-        }
-    }
+    let start_ends = actual_block_starts.windows(2);
+    start_ends
+        .map(|slice| match *slice {
+            [Some(start), Some(end)] => &file[start..end],
+            [Some(start), None] => &file[start..],
+            _ => panic!("Hmm how'd this happen?"),
+        })
+        .collect()
 }
 
 fn merge_maps(
@@ -173,11 +114,10 @@ fn merge_maps(
 pub fn write_cities<W: std::io::Write>(mut writer: BufWriter<W>) {
     // 8 threads is best for the desktop, 2 for the laptop
     let n_threads = 2;
-    let file_handles = (0..n_threads)
-        .collect::<Vec<u32>>()
-        .into_par_iter()
-        .map(|idx| split_up_file(get_file(), idx, n_threads));
+    let file = get_file();
+    let file_handles: Vec<&[u8]> = split_up_file(&file, n_threads);
     let mut cities: Vec<(String, City)> = file_handles
+        .into_par_iter()
         .flat_map(get_cities)
         .fold(FxHashMap::default, |mut hm, (name, city)| {
             let val = hm.entry(name).or_insert_with(City::new);
@@ -205,7 +145,10 @@ pub fn write_cities<W: std::io::Write>(mut writer: BufWriter<W>) {
 // Also I could just split the whole file into pieces and then give each piece to a thread.
 // Got options.
 fn read_row(row: &str) -> Row {
-    let semicolon_idx = memchr(b';', row.as_bytes()).unwrap();
+    let semicolon_idx = match memchr(b';', row.as_bytes()) {
+        Some(idx) => idx,
+        None => panic!("There was no semicolon; row was \"{}\"", row),
+    };
     let city = String::from(&row[..semicolon_idx]);
     let temp = String::from(if *row.as_bytes().last().unwrap() == b'\n' {
         &row[(semicolon_idx + 1)..(row.len() - 1)]
@@ -218,8 +161,8 @@ fn read_row(row: &str) -> Row {
     }
 }
 
-fn get_file() -> File {
-    File::open(FILENAME).unwrap()
+fn get_file() -> Mmap {
+    unsafe { memmap::Mmap::map(&File::open(FILENAME).unwrap()).unwrap() }
 }
 
 pub fn dumb_parse_number(s: &str) -> Temp {
@@ -258,57 +201,6 @@ pub fn dumb_parse_number(s: &str) -> Temp {
     get_output(builder, sign)
 }
 
-struct FilePortion<T: BufRead> {
-    reader: T,
-    max_offset_exclusive: u64,
-    current_offset: u64,
-}
-
-impl<T: BufRead> FilePortion<T> {
-    fn new(reader: T, max_offset_exclusive: u64) -> Self {
-        FilePortion {
-            reader,
-            current_offset: 0,
-            max_offset_exclusive,
-        }
-    }
-    fn remaining_bytes(&self) -> u64 {
-        self.max_offset_exclusive
-            .checked_sub(self.current_offset)
-            .and_then(|r| r.checked_sub(1))
-            .unwrap_or(0)
-    }
-}
-
-impl<T: BufRead> Read for FilePortion<T> {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let remaining_bytes = self.remaining_bytes() as usize;
-        let writable_buffer = if remaining_bytes >= buf.len() {
-            &mut buf[..]
-        } else {
-            &mut buf[..remaining_bytes]
-        };
-        self.reader.read(writable_buffer)
-    }
-}
-
-impl<T: BufRead> BufRead for FilePortion<T> {
-    fn fill_buf(&mut self) -> std::io::Result<&[u8]> {
-        let remaining_bytes = self.remaining_bytes() as usize;
-        let inner_buf = self.reader.fill_buf()?;
-        Ok(if inner_buf.len() > remaining_bytes {
-            &inner_buf[..remaining_bytes]
-        } else {
-            inner_buf
-        })
-    }
-
-    fn consume(&mut self, amt: usize) {
-        self.reader.consume(amt);
-        self.current_offset += amt as u64;
-    }
-}
-
 #[test]
 fn no_decimal() {
     assert_eq!(dumb_parse_number("14"), 14.0);
@@ -331,44 +223,17 @@ fn negative() {
 
 #[cfg(test)]
 mod reader_tests {
-    use crate::line_offsets;
     use crate::split_up_file;
-    use crate::FilePortion;
-    use std::fs::File;
-    use std::io::BufRead;
-    use std::io::BufReader;
-    use std::io::Seek;
-    use std::io::SeekFrom;
-    use std::io::Write;
-    use tempfile::tempfile;
-    const LINE_LENGTH: u64 = 10;
-    fn generate_test_file(n_lines: u64) -> File {
-        let mut file = tempfile().unwrap();
+    const LINE_LENGTH: usize = 10;
+    fn generate_test_file(n_lines: u64) -> String {
+        let mut out = String::new();
         for idx in 0..n_lines {
-            write!(file, "{}\n", format_number(idx)).unwrap();
+            out.push_str(&format!("{}\n", format_number(idx)));
         }
-        file.rewind().unwrap();
-        file
+        out
     }
     fn format_number(n: u64) -> String {
         format!("{:09}", n)
-    }
-
-    #[test]
-    fn read_big_file() {
-        let n_lines = 10_000;
-        let start_line: u64 = 440;
-        let length_lines = 2000;
-        let mut file = generate_test_file(n_lines);
-        file.seek(SeekFrom::Start((start_line * LINE_LENGTH).into()))
-            .unwrap();
-        let reader = BufReader::new(file);
-        let reader = FilePortion::new(reader, length_lines * LINE_LENGTH);
-        let expected: Vec<String> = (start_line..(start_line + length_lines))
-            .map(format_number)
-            .collect();
-        let actual: Vec<String> = reader.lines().map(|r| r.unwrap()).collect();
-        assert_eq!(expected, actual);
     }
 
     #[test]
@@ -376,30 +241,19 @@ mod reader_tests {
         let n_lines = 5000;
         let file = generate_test_file(n_lines);
         let n_chunks = 7;
-        let this_chunk = 4;
-        let expected_chunk_size = n_lines / n_chunks;
-        let reader = split_up_file(
-            file,
-            this_chunk.try_into().unwrap(),
-            n_chunks.try_into().unwrap(),
+        let actual_chunks: Vec<&[u8]> =
+            split_up_file(file.as_bytes(), n_chunks.try_into().unwrap());
+        assert_eq!(actual_chunks.len(), n_chunks);
+        let lengths: Vec<_> = actual_chunks.iter().map(|v| v.len()).collect();
+        assert_eq!(lengths.iter().sum::<usize>(), file.len());
+        let longest_length = *lengths.iter().max().unwrap();
+        let shortest_length = *lengths.iter().min().unwrap();
+        assert!(
+            (longest_length - shortest_length) * 10 < longest_length,
+            "The lengths are too different"
         );
-        let expected_lines: Vec<String> = ((this_chunk * expected_chunk_size)
-            ..((this_chunk + 1) * expected_chunk_size))
-            .map(format_number)
-            .collect();
-        let actual_lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-        assert_eq!(actual_lines, expected_lines);
-    }
 
-    #[test]
-    fn test_get_line_start_indexes() {
-        let n_lines = 100;
-        let file = generate_test_file(n_lines);
-        let expected_line_offsets: Vec<_> = (0..n_lines)
-            .map(|l| (LINE_LENGTH * l, LINE_LENGTH * (l + 1)))
-            .collect();
-        let actual_line_offsets: Vec<_> = line_offsets(file).collect();
-        assert_eq!(n_lines as usize, actual_line_offsets.len());
-        assert_eq!(expected_line_offsets, actual_line_offsets);
+        let recombined: Vec<u8> = actual_chunks.into_iter().flatten().copied().collect();
+        assert_eq!(&recombined, file.as_bytes());
     }
 }
